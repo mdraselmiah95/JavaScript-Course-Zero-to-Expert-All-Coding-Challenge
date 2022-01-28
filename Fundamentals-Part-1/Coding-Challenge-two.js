cdc0c88e368a2df26ec10b8939d0da664188e2ce
rasel <russelroy15@gmail.com>
2022-01-28 19:44:18 +0600

    challenge two solution

--- Fundamentals-Part-1/Coding-Challenge-two.js
+++ Fundamentals-Part-1/Coding-Challenge-two.js
@@ -5,3 +5,20 @@ Use the BMI example from Challenge #1, and the code you already wrote, and impro
 HINT: Use an if/else statement 😉
 GOOD LUCK 😀
 */
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`);
+}
