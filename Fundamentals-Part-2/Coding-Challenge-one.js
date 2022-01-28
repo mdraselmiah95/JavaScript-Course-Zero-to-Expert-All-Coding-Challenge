0fec12602c44a87fcce6a5f4b3c9892f0c567589
rasel <russelroy15@gmail.com>
2022-01-28 23:06:57 +0600

    new code added�

--- Fundamentals-Part-2/Coding-Challenge-one.js
+++ Fundamentals-Part-2/Coding-Challenge-one.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
 Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
