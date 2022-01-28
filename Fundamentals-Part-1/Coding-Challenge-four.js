65024ceec304064406975241e2420c9514ebec36
rasel <russelroy15@gmail.com>
2022-01-28 19:52:06 +0600

    challenge four solution

--- Fundamentals-Part-1/Coding-Challenge-four.js
+++ Fundamentals-Part-1/Coding-Challenge-four.js
@@ -7,3 +7,9 @@ HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
 HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 GOOD LUCK 😀
 */
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
