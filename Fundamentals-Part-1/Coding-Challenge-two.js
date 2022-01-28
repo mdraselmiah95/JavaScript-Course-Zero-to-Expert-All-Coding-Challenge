d4f718ce5cb7b79275e6db5eb09e3300bcd3f59f
rasel <russelroy15@gmail.com>
2022-01-28 19:44:33 +0600

    challenge two solution

--- Fundamentals-Part-1/Coding-Challenge-two.js
+++ Fundamentals-Part-1/Coding-Challenge-two.js
@@ -14,8 +14,10 @@ const heightJohn = 1.95;
 // const heightMark = 1.88;
 // const massJohn = 85;
 // const heightJohn = 1.76;
+
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
+
 console.log(BMIMark, BMIJohn);
 if (BMIMark > BMIJohn) {
   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
