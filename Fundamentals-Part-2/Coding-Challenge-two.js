7ab6332bfafe9d0e41b609b21d5207848aab4034
rasel <russelroy15@gmail.com>
2022-01-29 01:22:10 +0600

    exercise two solve

--- Fundamentals-Part-2/Coding-Challenge-two.js
+++ Fundamentals-Part-2/Coding-Challenge-two.js
@@ -17,3 +17,7 @@ const calcTip = function (bill) {
 // const tip = calcTip(100);
 // console.log(tip);
 //const calTip = (bill) => (bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2);
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
