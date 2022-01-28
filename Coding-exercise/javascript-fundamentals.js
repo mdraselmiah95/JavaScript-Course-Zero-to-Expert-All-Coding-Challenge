e9312017e31cfa50930eb67002ef4b63071f7957
rasel <russelroy15@gmail.com>
2022-01-29 01:07:39 +0600

    new code added

--- Coding-exercise/javascript-fundamentals.js
+++ Coding-exercise/javascript-fundamentals.js
@@ -83,3 +83,69 @@ const yearsUntilRetirement = function (birthYeah, firstName) {
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
 */
+
+//**************************Very Important **********************
+
+/*
+
+// Introduction to Arrays
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+const y = new Array(1991, 1984, 2008, 2020);
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice']
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
+
+// Basic Array Operations (Methods)
+const friends = ['Michael', 'Steven', 'Peter'];
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+friends.shift(); // First
+console.log(friends);
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+}
+*/
