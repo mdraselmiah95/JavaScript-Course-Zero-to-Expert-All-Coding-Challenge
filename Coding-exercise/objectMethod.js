0c97a8660a3cc5054e9284dca0a71bccccfd7fec
rasel <russelroy15@gmail.com>
2022-02-03 20:33:53 +0600

    WIP on javaScript Object method

---
+++ Coding-exercise/objectMethod.js
@@ -0,0 +1,13 @@
+// Object Methods
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYeah: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+  calcAge: function (birthYeah) {
+    return 2037 - birthYeah;
+  },
+};
