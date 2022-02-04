8ba82e4ea999bf32f7ba902f69ed3b291203587d
rasel <russelroy15@gmail.com>
2022-02-04 21:44:21 +0600

    WIP on JavaScript Loop concept

--- Coding-exercise/loop.js
+++ Coding-exercise/loop.js
@@ -77,9 +77,13 @@
 // }
 
 /////////////////////////////////////// For Loop
-for (let exercise = 1; exercise < 4; exercise++) {
-  console.log(`---------- Starting exercise ---------- ${exercise}`);
-  for (let rep = 1; rep < 6; rep++) {
-    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
-  }
-}
+// for (let exercise = 1; exercise < 4; exercise++) {
+//   console.log(`---------- Starting exercise ---------- ${exercise}`);
+//   for (let rep = 1; rep < 6; rep++) {
+//     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+//   }
+// }
+
+const dice = Math.random() * 6;
+
+console.log(dice);
