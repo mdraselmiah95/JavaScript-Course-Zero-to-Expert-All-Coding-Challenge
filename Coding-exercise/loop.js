abec4a7ab4b98ed98b71ff0d3d663099bb680d4c
rasel <russelroy15@gmail.com>
2022-02-04 21:16:35 +0600

    WIP on JavaScript Loop concept

--- Coding-exercise/loop.js
+++ Coding-exercise/loop.js
@@ -50,3 +50,10 @@ for (let i = 0; i < jonas.length; i++) {
   if (typeof jonas[i] !== "string") continue;
   console.log(jonas[i], typeof jonas[i]);
 }
+
+console.log("--- BREAK WITH NUMBER ---");
+
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+  console.log(jonas[i], typeof jonas[i]);
+}
