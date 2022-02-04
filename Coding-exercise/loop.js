e683d755c3df757c1cc07c463d07270ddf3fce43
rasel <russelroy15@gmail.com>
2022-02-04 21:33:36 +0600

    WIP on JavaScript Loop concept

--- Coding-exercise/loop.js
+++ Coding-exercise/loop.js
@@ -61,17 +61,25 @@
 
 ///////////////////////////////////////
 // Looping Backwards and Loops in Loops
-const jonas = [
-  "Jonas",
-  "Schmedtmann",
-  2037 - 1991,
-  "teacher",
-  ["Michael", "Peter", "Steven"],
-  true,
-];
-// 0, 1, ..., 4
-// 4, 3, ..., 0
+// const jonas = [
+//   "Jonas",
+//   "Schmedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+//   true,
+// ];
+// // 0, 1, ..., 4
+// // 4, 3, ..., 0
 
-for (i = jonas.length - 1; i >= 0; i--) {
-  console.log(i, jonas[i]);
+// for (i = jonas.length - 1; i >= 0; i--) {
+//   console.log(i, jonas[i]);
+// }
+
+/////////////////////////////////////// For Loop
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`---------- Starting exercise ---------- ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+  }
 }
