c2408f87ee77145cd3ed3cac10f0a1485f7257e7
rasel <russelroy15@gmail.com>
2022-02-04 20:51:34 +0600

    WIP on Javascript Loop lession

--- Coding-exercise/loop.js
+++ Coding-exercise/loop.js
@@ -1,4 +1,5 @@
 // for loop keeps running while condition is TRUE
-for (let rep = 1; rep <= 30; rep++) {
-  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
-}
+
+// for (let rep = 1; rep <= 30; rep++) {
+//   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+// }
