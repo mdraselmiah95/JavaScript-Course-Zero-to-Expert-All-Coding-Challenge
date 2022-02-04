23ec1e0416216ca4c1d9891f477dffc0bbb9dd30
rasel <russelroy15@gmail.com>
2022-02-04 20:52:13 +0600

    WIP on Javascript Loop lession

--- Coding-exercise/loop.js
+++ Coding-exercise/loop.js
@@ -3,3 +3,20 @@
 // for (let rep = 1; rep <= 30; rep++) {
 //   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 // }
+
+///////////////////////////////////////
+// Looping Arrays, Breaking and Continuing
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+// jonas[5] does NOT exist
