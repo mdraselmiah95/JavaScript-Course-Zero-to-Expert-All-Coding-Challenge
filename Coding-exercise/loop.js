d5ae7920071097ec3432b1e3c32aaf00728639a6
rasel <russelroy15@gmail.com>
2022-02-04 21:46:02 +0600

    WIP on JavaScript Loop concept

--- Coding-exercise/loop.js
+++ Coding-exercise/loop.js
@@ -94,6 +94,10 @@ while (rep <= 10) {
   rep++;
 }
 
-const dice = Math.trunc(Math.random() * 6) + 1;
+let dice = Math.trunc(Math.random() * 6) + 1;
 
-console.log(dice);
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
+}
