86a3ac3b137f3a1313568f07c3b31f07d561733b
rasel <russelroy15@gmail.com>
2022-02-05 03:00:42 +0600

    WIP on JavaScript Loop concept

--- Fundamentals-Part-2/Coding-Challenge-four.js
+++ Fundamentals-Part-2/Coding-Challenge-four.js
@@ -10,3 +10,7 @@
 //   4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
 //   4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
 //   4.3. Call the function with the 'totals' array
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
