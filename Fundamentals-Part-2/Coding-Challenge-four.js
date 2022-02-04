ea7cccf0dcb338bd58d3616cf6f75fae68c2f7cc
rasel <russelroy15@gmail.com>
2022-02-05 02:59:47 +0600

    WIP on JavaScript Loop concept

--- Fundamentals-Part-2/Coding-Challenge-four.js
+++ Fundamentals-Part-2/Coding-Challenge-four.js
@@ -5,3 +5,8 @@
 // TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
 
 // HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+// 4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+//   4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+//   4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+//   4.3. Call the function with the 'totals' array
