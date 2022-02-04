8e41c00ed9a189c3a83f9ac51ef0909448807647
rasel <russelroy15@gmail.com>
2022-02-05 02:59:13 +0600

    WIP on JavaScript Loop concept

--- Fundamentals-Part-2/Coding-Challenge-four.js
+++ Fundamentals-Part-2/Coding-Challenge-four.js
@@ -3,3 +3,5 @@
 // 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
 // 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
 // TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+// HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
