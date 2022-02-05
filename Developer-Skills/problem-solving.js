635cd42eacd4a3a9d4015bd0b34b4f6e96c497c3
rasel <russelroy15@gmail.com>
2022-02-05 20:00:55 +0600

    WIP on Problem solving

--- Developer-Skills/problem-solving.js
+++ Developer-Skills/problem-solving.js
@@ -18,3 +18,13 @@ const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 // - Find max value in temp array
 // - Find min value in temp array
 // - Subtract min from max (amplitude) and return it
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+};
