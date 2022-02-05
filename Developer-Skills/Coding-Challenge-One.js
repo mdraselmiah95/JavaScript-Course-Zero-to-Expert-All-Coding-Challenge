9a30c9b612ebe1116650bb3a412ce0b4c753bdeb
rasel <russelroy15@gmail.com>
2022-02-05 21:36:40 +0600

    WIP on Problem solving

--- Developer-Skills/Coding-Challenge-One.js
+++ Developer-Skills/Coding-Challenge-One.js
@@ -16,3 +16,7 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 // - Strings needs to contain day (index + 1)
 // - Add ... between elements and start and end of string
 // - Log string to console
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
