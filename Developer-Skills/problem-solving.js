2b04698f1aa204285e214e18531ed535911756e2
rasel <russelroy15@gmail.com>
2022-02-05 20:35:34 +0600

    WIP on Problem solving

--- Developer-Skills/problem-solving.js
+++ Developer-Skills/problem-solving.js
@@ -66,9 +66,17 @@ console.log(temperature);
 // Debugging with the Console and Breakpoints
 const measureKelvin = function () {
   const measurement = {
-    type: 'temp',
-    unit: 'celsius',
+    type: "temp",
+    unit: "celsius",
     // C) FIX
     // value: Number(prompt('Degrees celsius:')),
     value: 10,
   };
+  // B) FIND
+  console.table(measurement);
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
