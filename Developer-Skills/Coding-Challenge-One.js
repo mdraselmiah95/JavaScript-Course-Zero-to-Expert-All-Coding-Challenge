81187278545773185dd36dd42a8a5889d205a469
rasel <russelroy15@gmail.com>
2022-02-05 23:36:47 +0600

    WIP on Problem solving

--- Developer-Skills/Coding-Challenge-One.js
+++ Developer-Skills/Coding-Challenge-One.js
@@ -21,3 +21,12 @@ const data1 = [17, 21, 23];
 const data2 = [12, 5, -5, 0, 4];
 
 console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log("..." + str);
+};
+printForecast(data1);
