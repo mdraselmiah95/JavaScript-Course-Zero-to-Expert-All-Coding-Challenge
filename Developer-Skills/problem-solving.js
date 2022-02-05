06104cce326339e1d3b9192f192243c3720b8030
rasel <russelroy15@gmail.com>
2022-02-05 19:46:24 +0600

    WIP on Problem solving

--- Developer-Skills/problem-solving.js
+++ Developer-Skills/problem-solving.js
@@ -1,1 +1,9 @@
-console.log(11);
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+///////////////////////////////////////
+// Using Google, StackOverflow and MDN
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
