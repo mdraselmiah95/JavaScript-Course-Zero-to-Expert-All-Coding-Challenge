2add1767f86e7f5ac25e72dcf1b70d40df616145
rasel <russelroy15@gmail.com>
2022-02-06 22:24:06 +0600

    guess my number project

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -92,3 +92,15 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
 });
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+GOOD LUCK 😀
+*/
