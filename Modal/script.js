db34bc6d271ec34a67d1c81ff67061d23beccce4
rasel <russelroy15@gmail.com>
2022-02-07 21:55:24 +0600

    WIP on model project

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const model = document.querySelector(".model");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModel = document.querySelectorAll(".show-modal");
+
+console.log(model, overlay, btnCloseModal, btnOpenModel);
