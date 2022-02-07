b0b16e5e0dfe1af85f4a3b14d73bedf11ec61b7d
rasel <russelroy15@gmail.com>
2022-02-07 22:03:10 +0600

    WIP on model project

--- Modal/script.js
+++ Modal/script.js
@@ -17,5 +17,9 @@ for (let i = 0; i < btnOpenModel.length; i++) {
   btnOpenModel[i].addEventListener("click", openModel);
 
   btnCloseModal.addEventListener("click", closeModel);
-  overlay.addEventListener("click", closeModel);
+    overlay.addEventListener("click", closeModel);
+    
+    document.addEventListener("keydown", function (e)){
+        console.log(e.key)
+    }
 }
