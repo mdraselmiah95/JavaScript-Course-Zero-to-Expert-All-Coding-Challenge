3df393bf8cfa4612a1634615ea1ffdcb4b3adf22
rasel <russelroy15@gmail.com>
2022-02-08 07:57:57 +0600

    WIP on LUDO matching Game

--- Ludo-Matching-Game/script.js
+++ Ludo-Matching-Game/script.js
@@ -24,3 +24,7 @@ const btnHold = document.querySelector(".btn--hold");
 //   btnNew,
 //   btnRoll.btnHold
 // );
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
