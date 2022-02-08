127b4892f401236bfded2d6d430c001fb65fb896
rasel <russelroy15@gmail.com>
2022-02-08 08:28:50 +0600

    WIP on LUDO matching Game

--- Ludo-Matching-Game/script.js
+++ Ludo-Matching-Game/script.js
@@ -78,3 +78,33 @@ btnRoll.addEventListener("click", function () {
     }
   }
 });
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add("hidden");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
