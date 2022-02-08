73d2800a25adfb24e134df112bdbfcfe42ec3305
rasel <russelroy15@gmail.com>
2022-02-08 07:43:09 +0600

    WIP on LUDO matching Game

---
+++ Ludo-Matching-Game/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+console.log(
+  player0El,
+  player1El,
+  score0El,
+  score1El,
+  current0El,
+  current1El,
+  diceEl,
+  btnNew,
+  btnRoll.btnHold
+);
