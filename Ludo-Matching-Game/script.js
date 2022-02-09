dd3751dffca5cd5b698d8a127733c49d7b949a8b
rasel <russelroy15@gmail.com>
2022-02-09 22:31:05 +0600

    new code added

--- Ludo-Matching-Game/script.js
+++ Ludo-Matching-Game/script.js
@@ -76,6 +76,7 @@ btnRoll.addEventListener("click", function () {
     }
   }
 });
+
 btnHold.addEventListener("click", function () {
   if (playing) {
     // 1. Add current score to active player's score
