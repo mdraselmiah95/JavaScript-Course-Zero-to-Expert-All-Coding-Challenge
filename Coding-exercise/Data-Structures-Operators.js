ca53f201bba7d6e95489b54a02a6928e715e3fd7
rasel <russelroy15@gmail.com>
2022-02-12 11:17:30 +0600

    Data-Structures-Operators exercise

--- Coding-exercise/Data-Structures-Operators.js
+++ Coding-exercise/Data-Structures-Operators.js
@@ -71,5 +71,12 @@ const restaurant = {
 // [main, secondary] = [secondary, main];
 // console.log(main, secondary);
 
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
