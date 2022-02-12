36612e422ddd63dd5d45ac32d7828ad36e98fff6
rasel <russelroy15@gmail.com>
2022-02-12 12:14:07 +0600

    Data-Structures-Operators exercise

--- Coding-exercise/Data-Structures-Operators.js
+++ Coding-exercise/Data-Structures-Operators.js
@@ -59,6 +59,7 @@ const restaurant = {
 
 // SPREAD, because on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
+
 // REST, because on LEFT side of =
 const [a, b, ...others] = [1, 2, 3, 4, 5];
 console.log(a, b, others);
@@ -67,12 +68,29 @@ const [pizza, , risotto, ...otherFood] = [
   ...restaurant.starterMenu,
 ];
 console.log(pizza, risotto, otherFood);
+
 // Objects
 const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays);
 
 
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+const x = [23, 5, 7];
+add(...x);
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
 */
+
+/************************************/
 
 /*
 
