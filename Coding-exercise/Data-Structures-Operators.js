61f7fe0c7bb988c2c9159ec2576b0ea4394e8227
rasel <russelroy15@gmail.com>
2022-02-12 11:40:16 +0600

    Data-Structures-Operators exercise

--- Coding-exercise/Data-Structures-Operators.js
+++ Coding-exercise/Data-Structures-Operators.js
@@ -55,6 +55,51 @@ const restaurant = {
 /*
 
 ///////////////////////////////////////
+// The Spread Operator (...)
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+console.log(newMenu);
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+console.log(...str);
+// console.log(`${...str} Schmedtmann`);
+// Real-world example
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient 1?"),
+  // prompt('Ingredient 2?'),
+  // prompt('Ingredient 3'),
+];
+console.log(ingredients);
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Giuseppe' };
+console.log(newRestaurant);
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Restorable Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+
+
+*/
+
+/*
+
+///////////////////////////////////////
 // Destructuring Objects
 restaurant.orderDelivery({
   time: '22:30',
