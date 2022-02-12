171ec7c4f77605daccc87783a6a8ec4ff676a0d0
rasel <russelroy15@gmail.com>
2022-02-12 11:15:28 +0600

    Data-Structures-Operators exercise

--- Coding-exercise/Data-Structures-Operators.js
+++ Coding-exercise/Data-Structures-Operators.js
@@ -1,20 +1,20 @@
 "use strict";
 
 const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekdays[5]]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekdays[5]]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
 
 // console.log(weekdays, openingHours);
 
@@ -26,7 +26,7 @@ const restaurant = {
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
   // ES6 enhanced object literals
-  openingHours,
+  //   openingHours,
 
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
