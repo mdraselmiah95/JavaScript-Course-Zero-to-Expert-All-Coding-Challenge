e114e8d79eccab28046c9b45d99960589a602e77
rasel <russelroy15@gmail.com>
2022-02-12 11:05:52 +0600

    Data-Structures-Operators exercise

--- Coding-exercise/Data-Structures-Operators.js
+++ Coding-exercise/Data-Structures-Operators.js
@@ -1,1 +1,17 @@
-console.log(11);
+"use strict";
+
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
