adbba4f864cfe099548901a7f841f3b93b78868c
rasel <russelroy15@gmail.com>
2022-02-13 09:03:02 +0600

    WIP on Data Structures Operators Coding Challenge one

--- Data-Structures-Operators/Coding-Challenge-one.js
+++ Data-Structures-Operators/Coding-Challenge-one.js
@@ -11,7 +11,7 @@ with the goalkeeper's name, and one array ('fieldPlayers') with all the remainin
 
 3. Create an array 'allPlayers' containing all players of both teams (22 players)
 
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Things', 'Continuo' and 'Persist'
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 
 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 
@@ -75,4 +75,8 @@ const [gk, ...fieldPlayers] = players1;
 
 //3.
 const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// console.log(allPlayers);
+
+//4.
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
