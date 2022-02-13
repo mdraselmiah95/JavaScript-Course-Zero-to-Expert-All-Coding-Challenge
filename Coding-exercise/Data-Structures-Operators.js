cdc9cdea7ea11f0c4307cd9423e21e2058ba2af7
rasel <russelroy15@gmail.com>
2022-02-13 07:32:04 +0600

    WIP on Data Structures Operators exercise

--- Coding-exercise/Data-Structures-Operators.js
+++ Coding-exercise/Data-Structures-Operators.js
@@ -52,7 +52,39 @@ const restaurant = {
 
 // console.log(restaurant);
 
-console.log(11);
+/*
+
+
+///////////////////////////////////////
+// Short Circuiting (&& and ||)
+
+console.log('---- OR ----');
+
+// Use ANY data type, return ANY data type, short-circuiting
+
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+
+*/
 
 ///////////////////////////////////////
 // Rest Pattern and Parameters
