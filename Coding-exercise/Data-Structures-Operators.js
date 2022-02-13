250d3ffe45de56f93a52bceb08c7d381300e4f6e
rasel <russelroy15@gmail.com>
2022-02-13 07:32:45 +0600

    WIP on Data Structures Operators exercise

--- Coding-exercise/Data-Structures-Operators.js
+++ Coding-exercise/Data-Structures-Operators.js
@@ -54,6 +54,21 @@ const restaurant = {
 
 /*
 
+///////////////////////////////////////
+
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+*/
+
+/*
+
 
 ///////////////////////////////////////
 // Short Circuiting (&& and ||)
