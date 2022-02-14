34aaf05decd5d99b96323c46abcf21b48173e9a6
rasel <russelroy15@gmail.com>
2022-02-14 15:29:21 +0600

    WIP on Coding challenge and solution

--- Data-Structures-Operators/Coding-Challenge-three.js
+++ Data-Structures-Operators/Coding-Challenge-three.js
@@ -45,3 +45,9 @@ console.log(time);
 console.log(
   `An event happened, on average, every ${time / gameEvents.size} minutes`
 );
+
+//4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
