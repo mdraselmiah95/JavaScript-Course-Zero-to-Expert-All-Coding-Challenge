57c714c31f5d38df96f147d29f48c7fe17b82b5f
rasel <russelroy15@gmail.com>
2022-02-14 08:09:24 +0600

    WIP on coding Challenge and solution

--- Data-Structures-Operators/Coding-Challenge-two.js
+++ Data-Structures-Operators/Coding-Challenge-two.js
@@ -71,3 +71,9 @@ let average = 0;
 for (const odd of odds) average += odd;
 average /= odds.length;
 console.log(average);
+
+//3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
