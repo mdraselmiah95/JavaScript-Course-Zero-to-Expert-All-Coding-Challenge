e63367e921f36ca02d5751c4e9741c414cff8c27
rasel <russelroy15@gmail.com>
2022-02-14 07:30:44 +0600

    WIP on coding exercise

--- Coding-exercise/Data-Structures-Operators.js
+++ Coding-exercise/Data-Structures-Operators.js
@@ -49,8 +49,36 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+/*******************************************************************/
+
+/*
+
+///////////////////////////////////////
+// Looping Objects: Object Keys, Values, and Entries
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
 
 
+*/
 
 /*
 
@@ -92,11 +120,6 @@ for (const [i, el] of menu.entries()) {
 }
 // console.log([...menu.entries()]);
 */
-
-
-*/
-
-
 
 /*
 
