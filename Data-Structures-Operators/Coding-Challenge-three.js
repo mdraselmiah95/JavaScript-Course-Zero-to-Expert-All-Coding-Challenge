8ff0dea9bd40deea87aedc4a8c688f3050173a52
rasel <russelroy15@gmail.com>
2022-02-14 15:19:21 +0600

    WIP on Coding challenge and solution

--- Data-Structures-Operators/Coding-Challenge-three.js
+++ Data-Structures-Operators/Coding-Challenge-three.js
@@ -13,3 +13,17 @@ Let's continue with our football betting app! This time, we have a map with a lo
       [FIRST HALF] 17: ⚽️ GOAL
 GOOD LUCK 😀
 */
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
