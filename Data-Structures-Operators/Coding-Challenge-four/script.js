fe21488706fc5ba90b7b286f124ba9bbbe0459f8
rasel <russelroy15@gmail.com>
2022-02-15 21:05:49 +0600

    WIP on Coding Challenge and solution

--- Data-Structures-Operators/Coding-Challenge-four/script.js
+++ Data-Structures-Operators/Coding-Challenge-four/script.js
@@ -31,3 +31,8 @@ GOOD LUCK 😀
 
 document.body.append(document.createElement("textarea"));
 document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  console.log(text);
+});
