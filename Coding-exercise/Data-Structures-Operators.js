eacc62e396e8a61c1896de9603e8ef6e1bfce3f4
rasel <russelroy15@gmail.com>
2022-02-15 20:39:54 +0600

    WIP on Coding exercise

--- Coding-exercise/Data-Structures-Operators.js
+++ Coding-exercise/Data-Structures-Operators.js
@@ -51,9 +51,14 @@ const restaurant = {
 };
 /*******************************************************************/
 
-const message = "Go to gate 23!";
-console.log(message.padStart(20, "+").padEnd(30, "+"));
-console.log("Jonas".padStart(20, "+").padEnd(30, "+"));
+const maskCreditCard = function (number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+};
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard("334859493847755774747"));
 /*
 ///////////////////////////////////////
 // Working With Strings - Part 2
