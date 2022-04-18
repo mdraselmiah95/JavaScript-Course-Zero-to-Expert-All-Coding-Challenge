6bbcb3b8b62913e96e8730275214dcf09ea18725
mdraselmiah95 <russelroy15@gmail.com>
2022-04-19 04:30:15 +0600

    WIP on Function learning

---
+++ Functions/Coding-exercise.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Default Parameters
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers, price) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
