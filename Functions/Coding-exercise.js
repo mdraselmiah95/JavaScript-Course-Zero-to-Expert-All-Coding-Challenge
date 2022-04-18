de416e80f39d9476b06f617af5e4d7abd87f95a9
mdraselmiah95 <russelroy15@gmail.com>
2022-04-19 04:32:09 +0600

    WIP on Function learning

--- Functions/Coding-exercise.js
+++ Functions/Coding-exercise.js
@@ -18,3 +18,7 @@ const createBooking = function (flightNum, numPassengers = 1, price = 190) {
 };
 
 createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 2);
+createBooking("LH123", 5);
+createBooking("LH123", undefined, 1000);
