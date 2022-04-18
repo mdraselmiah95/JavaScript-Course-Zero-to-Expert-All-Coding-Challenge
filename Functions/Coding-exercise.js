f94b5bcf4d802bac71be6f0714f6c9068adad5d6
mdraselmiah95 <russelroy15@gmail.com>
2022-04-19 04:37:31 +0600

    WIP on Function learning

--- Functions/Coding-exercise.js
+++ Functions/Coding-exercise.js
@@ -26,3 +26,22 @@ createBooking("LH123", 2, 800);
 createBooking("LH123", 2);
 createBooking("LH123", 5);
 createBooking("LH123", undefined, 1000);
+
+// How Passing Arguments Works: Values vs. Reference
+const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 24739479284,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
+  if (passenger.passport === 24739479284) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
