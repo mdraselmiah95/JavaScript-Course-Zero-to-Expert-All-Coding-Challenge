00d595520882aa520355ba8a8a52fdbc60155a10
mdraselmiah95 <russelroy15@gmail.com>
2022-04-19 17:46:45 +0600

    WIP on Function learning

--- Functions/Coding-exercise.js
+++ Functions/Coding-exercise.js
@@ -21,11 +21,11 @@ const createBooking = function (
   bookings.push(booking);
 };
 
-createBooking("LH123");
-createBooking("LH123", 2, 800);
-createBooking("LH123", 2);
-createBooking("LH123", 5);
-createBooking("LH123", undefined, 1000);
+// createBooking("LH123");
+// createBooking("LH123", 2, 800);
+// createBooking("LH123", 2);
+// createBooking("LH123", 5);
+// createBooking("LH123", undefined, 1000);
 
 // How Passing Arguments Works: Values vs. Reference
 const flight = "LH234";
@@ -42,6 +42,24 @@ const checkIn = function (flightNum, passenger) {
     console.log("Wrong passport!");
   }
 };
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
