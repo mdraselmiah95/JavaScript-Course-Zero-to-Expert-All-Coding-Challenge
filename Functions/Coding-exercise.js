7a3afc21787cadbcc640311f4a290450b3c2f9cd
mdraselmiah95 <russelroy15@gmail.com>
2022-04-19 17:55:42 +0600

    WIP on Function learning

--- Functions/Coding-exercise.js
+++ Functions/Coding-exercise.js
@@ -63,3 +63,10 @@ const transformer = function (str, fn) {
 };
 transformer("JavaScript is the best!", upperFirstWord);
 transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log("👋");
+};
+document.body.addEventListener("click", high5);
+["Jonas", "Martha", "Adam"].forEach(high5);
