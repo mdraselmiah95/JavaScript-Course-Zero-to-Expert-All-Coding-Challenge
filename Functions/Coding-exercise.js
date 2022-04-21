d729af27eaa640a838c62b189d729f288114ac50
mdraselmiah95 <russelroy15@gmail.com>
2022-04-21 17:31:04 +0600

    WIP on Function learning

--- Functions/Coding-exercise.js
+++ Functions/Coding-exercise.js
@@ -176,3 +176,22 @@ const addTaxRate = function (rate) {
 const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
+
+// Immediately Invoked Function Expressions (IIFE)
+const runOnce = function () {
+  console.log("This will never run again");
+};
+runOnce();
+// IIFE
+(function () {
+  console.log("This will never run again");
+  const isPrivate = 23;
+})();
+// console.log(isPrivate);
+(() => console.log("This will ALSO never run again"))();
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
--- Functions/codingChallengeOne.js
+++ Functions/codingChallengeOne.js
@@ -25,7 +25,7 @@ GOOD LUCK 😀
 */
 
 const poll = {
-  question: "What is your favourite programming language?",
+  question: "What is your favorite programming language?",
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
