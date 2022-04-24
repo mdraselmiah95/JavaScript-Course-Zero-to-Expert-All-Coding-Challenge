0eb5a24dd998b5461528d1b681a10b362b4f0892
mdraselmiah95 <russelroy15@gmail.com>
2022-04-25 02:58:01 +0600

    WIP on Function learning

--- Functions/Coding-exercise.js
+++ Functions/Coding-exercise.js
@@ -219,3 +219,17 @@ const g = function () {
     console.log(a * 2);
   };
 };
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+console.dir(f);
+// Re-assigning f function
+h();
+f();
+console.dir(f);
