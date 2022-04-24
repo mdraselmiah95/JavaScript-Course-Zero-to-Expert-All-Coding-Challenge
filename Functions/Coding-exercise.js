83120c14830001d0182dad4137047f28d6d62a40
mdraselmiah95 <russelroy15@gmail.com>
2022-04-25 03:10:55 +0600

    WIP on Function learning

--- Functions/Coding-exercise.js
+++ Functions/Coding-exercise.js
@@ -233,3 +233,15 @@ console.dir(f);
 h();
 f();
 console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+const perGroup = 1000;
+boardPassengers(180, 3);
