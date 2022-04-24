cd941f9c0004c89e0f1fd04c9eccd9416223dd19
mdraselmiah95 <russelroy15@gmail.com>
2022-04-25 03:17:27 +0600

    WIP on Function learning

--- Functions/codingChallenge.js
+++ Functions/codingChallenge.js
@@ -70,3 +70,11 @@ Take the IIFE below and at the end of the function, attach an event listener tha
 And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
 GOOD LUCK 😀
 */
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
