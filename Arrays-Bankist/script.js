e34ee268256d4309137caa649283ed23b0e165b4
mdraselmiah95 <russelroy15@gmail.com>
2022-04-28 04:15:38 +0600

    WIP on function coding challenge

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -138,3 +138,15 @@ const movementsDescriptions = movements.map(
     )}`
 );
 console.log(movementsDescriptions);
+
+// The filter Method
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+const withdrawals = movements.filter((mov) => mov < 0);
+console.log(withdrawals);
