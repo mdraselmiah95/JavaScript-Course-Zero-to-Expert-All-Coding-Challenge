17bdd5462ad4d1cc6ebf9989dbf7a40c56032bb1
mdraselmiah95 <russelroy15@gmail.com>
2022-04-28 04:17:48 +0600

    WIP on function coding challenge

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -150,3 +150,22 @@ for (const mov of movements) if (mov > 0) depositsFor.push(mov);
 console.log(depositsFor);
 const withdrawals = movements.filter((mov) => mov < 0);
 console.log(withdrawals);
+
+// The reduce Method
+console.log(movements);
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
