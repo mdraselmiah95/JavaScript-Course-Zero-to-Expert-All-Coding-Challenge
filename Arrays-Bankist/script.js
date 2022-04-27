74afdff489af6fb25ab4584e2b3456b32e2a7ac6
mdraselmiah95 <russelroy15@gmail.com>
2022-04-28 04:14:32 +0600

    WIP on function coding challenge

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -119,3 +119,22 @@ const checkDogs = function (dogsJulia, dogsKate) {
 };
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// The map Method
+const eurToUsd = 1.1;
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+const movementsUSD = movements.map((mov) => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUSD);
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
