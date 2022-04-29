c9a6a380e76faaf5a7ceaa450a24a389b0552059
mdraselmiah95 <russelroy15@gmail.com>
2022-04-29 20:52:30 +0600

    new code added

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -134,6 +134,35 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+///////////////////////////////////////
+// Event handlers
+let currentAccount;
+
+btnLogin.addEventListener("click", function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur();
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
 // Coding Challenge #1
 
 /* 
