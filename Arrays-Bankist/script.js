49c52606aa4d5b7f771b82c64302e5241732335b
mdraselmiah95 <russelroy15@gmail.com>
2022-04-29 20:52:44 +0600

    new code added

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -163,6 +163,28 @@ btnLogin.addEventListener("click", function (e) {
   }
 });
 
+btnTransfer.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = "";
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
 // Coding Challenge #1
 
 /* 
