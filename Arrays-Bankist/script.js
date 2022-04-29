53247499e3e295626e4bbd2f2de5eb11bc8485a2
mdraselmiah95 <russelroy15@gmail.com>
2022-04-29 20:45:39 +0600

    new code added

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -162,46 +162,46 @@ const eurToUsd = 1.1;
 //   return mov * eurToUsd;
 // });
 const movementsUSD = movements.map((mov) => mov * eurToUsd);
-console.log(movements);
-console.log(movementsUSD);
+// console.log(movements);
+// console.log(movementsUSD);
 const movementsUSDfor = [];
 for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
-console.log(movementsUSDfor);
+// console.log(movementsUSDfor);
 const movementsDescriptions = movements.map(
   (mov, i) =>
     `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
       mov
     )}`
 );
-console.log(movementsDescriptions);
+// console.log(movementsDescriptions);
 
 // The filter Method
 const deposits = movements.filter(function (mov, i, arr) {
   return mov > 0;
 });
-console.log(movements);
-console.log(deposits);
+// console.log(movements);
+// console.log(deposits);
 const depositsFor = [];
 for (const mov of movements) if (mov > 0) depositsFor.push(mov);
-console.log(depositsFor);
+// console.log(depositsFor);
 const withdrawals = movements.filter((mov) => mov < 0);
-console.log(withdrawals);
+// console.log(withdrawals);
 
 // The reduce Method
-console.log(movements);
+// console.log(movements);
 // accumulator -> SNOWBALL
 // const balance = movements.reduce(function (acc, cur, i, arr) {
 //   console.log(`Iteration ${i}: ${acc}`);
 //   return acc + cur;
 // }, 0);
 const balance = movements.reduce((acc, cur) => acc + cur, 0);
-console.log(balance);
+// console.log(balance);
 let balance2 = 0;
 for (const mov of movements) balance2 += mov;
-console.log(balance2);
+// console.log(balance2);
 // Maximum value
 const max = movements.reduce((acc, mov) => {
   if (acc > mov) return acc;
   else return mov;
 }, movements[0]);
-console.log(max);
+// console.log(max);
