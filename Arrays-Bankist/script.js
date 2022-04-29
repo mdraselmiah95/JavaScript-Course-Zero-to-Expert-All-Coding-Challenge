689aac8c06bc3c742e523a96fb7cc07b21b2239d
mdraselmiah95 <russelroy15@gmail.com>
2022-04-29 20:53:32 +0600

    new code added

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -204,6 +204,29 @@ btnLoan.addEventListener("click", function (e) {
   inputLoanAmount.value = "";
 });
 
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    console.log(index);
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = "";
+});
+
 // Coding Challenge #1
 
 /* 
