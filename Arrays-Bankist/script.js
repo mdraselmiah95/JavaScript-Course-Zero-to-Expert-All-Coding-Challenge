828349aa9f095666a0579af17d9ec1d006d2b4c8
mdraselmiah95 <russelroy15@gmail.com>
2022-05-01 04:43:21 +0600

    new question and answers

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -412,3 +412,20 @@ console.log(firstWithdrawal);
 console.log(accounts);
 const account = accounts.find((acc) => acc.owner === "Jessica Davis");
 console.log(account);
+
+// some and every
+console.log(movements);
+// EQUALITY
+console.log(movements.includes(-130));
+// SOME: CONDITION
+console.log(movements.some((mov) => mov === -130));
+const anyDeposits = movements.some((mov) => mov > 0);
+console.log(anyDeposits);
+// EVERY
+console.log(movements.every((mov) => mov > 0));
+console.log(account4.movements.every((mov) => mov > 0));
+// Separate callback
+const deposit = (mov) => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
