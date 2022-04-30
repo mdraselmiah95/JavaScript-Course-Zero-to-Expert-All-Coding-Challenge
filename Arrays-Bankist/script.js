4c03b9ceb746f48f5ba1d1278af60e4ea3523715
mdraselmiah95 <russelroy15@gmail.com>
2022-05-01 04:42:17 +0600

    new question and answers

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -370,13 +370,12 @@ const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
 
-
 // The Magic of Chaining Methods
-const eurToUsd = 1.1;
+
 console.log(movements);
 // PIPELINE
 const totalDepositsUSD = movements
-  .filter(mov => mov > 0)
+  .filter((mov) => mov > 0)
   .map((mov, i, arr) => {
     // console.log(arr);
     return mov * eurToUsd;
@@ -384,4 +383,12 @@ const totalDepositsUSD = movements
   // .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK 😀
 */
