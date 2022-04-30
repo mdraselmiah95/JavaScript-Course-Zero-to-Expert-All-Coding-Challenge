8a9ed09be8e485e482446b038ba838a919a53a0d
mdraselmiah95 <russelroy15@gmail.com>
2022-05-01 04:43:50 +0600

    new question and answers

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -446,3 +446,20 @@ const overalBalance2 = accounts
   .flatMap((acc) => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2);
+
+// Sorting Arrays
+// Strings
+const owners = ["Jonas", "Zach", "Adam", "Martha"];
+console.log(owners.sort());
+console.log(owners);
+// Numbers
+console.log(movements);
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
