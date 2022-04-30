269e4d6528b76d4954e670ceada00ef970322cd4
mdraselmiah95 <russelroy15@gmail.com>
2022-05-01 04:41:46 +0600

    new question and answers

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -369,3 +369,19 @@ const calcAverageHumanAge = function (ages) {
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
+
+
+// The Magic of Chaining Methods
+const eurToUsd = 1.1;
+console.log(movements);
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+*/
