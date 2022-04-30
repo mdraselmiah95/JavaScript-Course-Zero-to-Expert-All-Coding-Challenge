63b717d7e60f0ed47789b13263e66e6ce6422fcd
mdraselmiah95 <russelroy15@gmail.com>
2022-05-01 04:44:45 +0600

    new question and answers

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -475,7 +475,7 @@ console.log(movements);
 // More Ways of Creating and Filling Arrays
 // const arr = [1, 2, 3, 4, 5, 6, 7];
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
-// Emprty arrays + fill method
+// Empty arrays + fill method
 const x = new Array(7);
 console.log(x);
 // console.log(x.map(() => 5));
@@ -484,3 +484,17 @@ x.fill(1);
 console.log(x);
 arr.fill(23, 2, 6);
 console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+labelBalance.addEventListener("click", function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll(".movements__value"),
+    (el) => Number(el.textContent.replace("€", ""))
+  );
+  console.log(movementsUI);
+  const movementsUI2 = [...document.querySelectorAll(".movements__value")];
+});
