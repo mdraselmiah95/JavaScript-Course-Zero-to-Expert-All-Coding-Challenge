d9e61e0e1253da253495a76441b7fd8edf70d412
mdraselmiah95 <russelroy15@gmail.com>
2022-05-01 04:45:46 +0600

    new question and answers

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -524,3 +524,19 @@ console.log(numDeposits1000);
 let a = 10;
 console.log(++a);
 console.log(a);
+
+/*
+// 3.
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+
+console.log(deposits, withdrawals);
+*/
