f90a25574a20c7398c2f8509b2b5a498eb198cf1
mdraselmiah95 <russelroy15@gmail.com>
2022-05-01 04:46:10 +0600

    new question and answers

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -540,3 +540,23 @@ const { deposits, withdrawals } = accounts
 
 console.log(deposits, withdrawals);
 */
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ["a", "an", "and", "the", "but", "or", "on", "in", "with"];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(" ")
+    .map((word) => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(" ");
+
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase("this is a nice title"));
+console.log(convertTitleCase("this is a LONG title but not too long"));
+console.log(convertTitleCase("and here is another title with an EXAMPLE"));
