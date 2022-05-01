822cb031b4fd49f46b525127a8c7abd63f2ddfbf
mdraselmiah95 <russelroy15@gmail.com>
2022-05-02 04:22:33 +0600

    new challenge and soluation

--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -584,3 +584,10 @@ const dogs = [
 ];
 GOOD LUCK 😀
 */
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
