0cf8242bbc40f4fa2640f9c79f8efa8221037220
mdraselmiah95 <russelroy15@gmail.com>
2022-05-05 20:09:35 +0600

    WIP on Number Date and Times

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -396,3 +396,10 @@ console.log(Number.isInteger(23 / 0));
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
 console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, "23", 11, 2));
+console.log(Math.max(5, 18, "23px", 11, 2));
+console.log(Math.min(5, 18, 23, 11, 2));
+console.log(Math.PI * Number.parseFloat("10px") ** 2);
+console.log(Math.trunc(Math.random() * 6) + 1);
