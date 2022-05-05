e6f349f186c5b9838dd24edd378c66732c48f9ea
mdraselmiah95 <russelroy15@gmail.com>
2022-05-05 20:17:39 +0600

    WIP on Number Date and Times

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -460,3 +460,13 @@ const PI = 3.1415;
 console.log(PI);
 console.log(Number("230_000"));
 console.log(parseInt("230_000"));
+
+// Working with BigInt
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+console.log(4838430248342043823408394839483204n);
+console.log(BigInt(48384302));
