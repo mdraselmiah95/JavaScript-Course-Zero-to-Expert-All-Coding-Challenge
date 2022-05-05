c0eb07309ed87fd0211aff57c59760a70ebdb330
mdraselmiah95 <russelroy15@gmail.com>
2022-05-05 19:58:02 +0600

    WIP on Number Date and Times

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -377,3 +377,17 @@ console.log(Number.parseInt("e23", 10));
 console.log(Number.parseInt("  2.5rem  "));
 console.log(Number.parseFloat("  2.5rem  "));
 // console.log(parseFloat('  2.5rem  '));
+
+// Check if value is NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN("20"));
+console.log(Number.isNaN(+"20X"));
+console.log(Number.isNaN(23 / 0));
+// Checking if value is number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite("20"));
+console.log(Number.isFinite(+"20X"));
+console.log(Number.isFinite(23 / 0));
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
