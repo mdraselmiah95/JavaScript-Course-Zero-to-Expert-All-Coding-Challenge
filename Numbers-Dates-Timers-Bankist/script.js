a0452c73584f845cacc3b7f69a03603e3473ab01
mdraselmiah95 <russelroy15@gmail.com>
2022-05-05 20:14:54 +0600

    WIP on Number Date and Times

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -403,3 +403,18 @@ console.log(Math.max(5, 18, "23px", 11, 2));
 console.log(Math.min(5, 18, 23, 11, 2));
 console.log(Math.PI * Number.parseFloat("10px") ** 2);
 console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0...1 -> 0...(max - min) -> min...max
+// console.log(randomInt(10, 20));
+// Rounding integers
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+console.log(Math.floor(23.3));
+console.log(Math.floor("23.9"));
+console.log(Math.trunc(23.3));
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
