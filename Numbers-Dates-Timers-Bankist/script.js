d79314040e3349d8af0c1631d5a3c677bab1c675
mdraselmiah95 <russelroy15@gmail.com>
2022-05-05 20:17:24 +0600

    WIP on Number Date and Times

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -447,3 +447,16 @@ labelBalance.addEventListener("click", function () {
     if (i % 3 === 0) row.style.backgroundColor = "blue";
   });
 });
+
+// Numeric Separators
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+const price = 345_99;
+console.log(price);
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+const PI = 3.1415;
+console.log(PI);
+console.log(Number("230_000"));
+console.log(parseInt("230_000"));
