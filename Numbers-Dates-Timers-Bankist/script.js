5520fa94b69a539b7e8e3a1970e57d6969f9dbd2
mdraselmiah95 <russelroy15@gmail.com>
2022-05-05 19:57:43 +0600

    WIP on Number Date and Times

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -359,3 +359,21 @@ btnSort.addEventListener("click", function (e) {
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
 });
+
+// LECTURES
+
+// Converting and Checking Numbers
+console.log(23 === 23.0);
+// Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.3333333
+// Binary base 2 - 0 1
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+// Conversion
+console.log(Number("23"));
+console.log(+"23");
+// Parsing
+console.log(Number.parseInt("30px", 10));
+console.log(Number.parseInt("e23", 10));
+console.log(Number.parseInt("  2.5rem  "));
+console.log(Number.parseFloat("  2.5rem  "));
+// console.log(parseFloat('  2.5rem  '));
