d2b09dbd62a2a0f820ea99893fc1af731d39275d
mdraselmiah95 <russelroy15@gmail.com>
2022-05-05 20:15:30 +0600

    WIP on Number Date and Times

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -424,3 +424,17 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+// The Remainder Operator
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+console.log(6 % 2);
+console.log(6 / 2);
+console.log(7 % 2);
+console.log(7 / 2);
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
