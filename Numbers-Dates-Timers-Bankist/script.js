3a3a35a601ebc8c2162d79a30694a638a8c1435d
mdraselmiah95 <russelroy15@gmail.com>
2022-05-06 18:26:18 +0600

    WIP on Numbers Dates and Time

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -522,3 +522,12 @@ future.setFullYear(2040);
 console.log(future);
 
 // Operations With Dates
+
+/*
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
+*/
