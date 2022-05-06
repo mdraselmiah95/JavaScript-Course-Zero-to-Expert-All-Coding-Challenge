1f54737e67f8aafbc0273ce1d63dbcdf391d1a92
mdraselmiah95 <russelroy15@gmail.com>
2022-05-07 02:15:50 +0600

    new code added

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -559,8 +559,11 @@ const pizzaTimer = setTimeout(
 );
 console.log("Waiting...");
 if (ingredients.includes("spinach")) clearTimeout(pizzaTimer);
+
 // setInterval
 setInterval(function () {
   const now = new Date();
   console.log(now);
 }, 1000);
+
+//code end
