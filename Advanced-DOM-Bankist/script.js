987b16a56507454cc0b6be964a9926bcfc5a007e
mdraselmiah95 <russelroy15@gmail.com>
2022-05-10 23:38:40 +0600

    WIP on Advance DOM project

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -64,3 +64,14 @@ btnScrollTo.addEventListener("click", function (e) {
 
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
