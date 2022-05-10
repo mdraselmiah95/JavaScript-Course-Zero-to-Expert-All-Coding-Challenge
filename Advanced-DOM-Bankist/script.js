06e7a04717b2a5397517b64586b440794226bb32
mdraselmiah95 <russelroy15@gmail.com>
2022-05-10 23:34:30 +0600

    WIP on Advance DOM project

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -34,3 +34,19 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// Button scrolling
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+});
