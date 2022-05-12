8cdf7c748b67f268f02ad526b4145ac9063d4bc8
mdraselmiah95 <russelroy15@gmail.com>
2022-05-12 23:57:41 +0600

    menu fade animation added

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -109,3 +109,17 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
