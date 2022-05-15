f03f1b57fade8a1c2ae1d747ccc44adcc9bfd003
mdraselmiah95 <russelroy15@gmail.com>
2022-05-15 21:21:14 +0600

    WIP on the advance javascript project

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -161,3 +161,13 @@ const revealSection = function (entries, observer) {
   entry.target.classList.remove("section--hidden");
   observer.unobserve(entry.target);
 };
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
