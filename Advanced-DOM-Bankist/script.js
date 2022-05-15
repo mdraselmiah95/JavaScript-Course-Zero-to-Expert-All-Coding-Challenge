e4566b638f3d51b95697b19e9b68aa0dde621593
mdraselmiah95 <russelroy15@gmail.com>
2022-05-15 21:19:04 +0600

    WIP on the advance javascript project

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -141,3 +141,11 @@ const stickyNav = function (entries) {
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
 };
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
