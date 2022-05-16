ad63485d84128cfeaabfae0c56fde70eed3dc495
mdraselmiah95 <russelroy15@gmail.com>
2022-05-16 23:49:25 +0600

    Slider  added

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -233,5 +233,35 @@ const slider = function () {
       (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
     );
   };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
 };
 slider();
