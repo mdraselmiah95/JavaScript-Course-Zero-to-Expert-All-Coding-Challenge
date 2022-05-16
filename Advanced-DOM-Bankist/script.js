c6f08fa613bc5761d45ea4dde59820e835a78b4e
mdraselmiah95 <russelroy15@gmail.com>
2022-05-16 23:48:40 +0600

    Slider  added

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -197,3 +197,25 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach((img) => imgObserver.observe(img));
+
+// Slider
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+};
+slider();
