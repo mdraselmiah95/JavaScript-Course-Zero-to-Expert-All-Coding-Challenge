7d5a7dfc325b91bcd8b71f730a5c77e5d344401a
mdraselmiah95 <russelroy15@gmail.com>
2022-05-17 08:33:46 +0600

    WIP on advance javascript

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -308,3 +308,16 @@ message.classList.add("cookie-message");
 message.innerHTML =
   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 */
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+// Delete elements
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
