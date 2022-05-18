6df4c2575dc19009818b45f0a44a46a3a9baacf5
mdraselmiah95 <russelroy15@gmail.com>
2022-05-18 11:31:08 +0600

    JavaScript Attributes learnign

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -340,3 +340,13 @@ const logo = document.querySelector(".nav__logo");
 console.log(logo.alt);
 console.log(logo.className);
 logo.alt = "Beautiful minimalist logo";
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute("designer"));
+logo.setAttribute("company", "Bankist");
+console.log(logo.src);
+console.log(logo.getAttribute("src"));
+const link = document.querySelector(".nav__link--btn");
+console.log(link.href);
+console.log(link.getAttribute("href"));
