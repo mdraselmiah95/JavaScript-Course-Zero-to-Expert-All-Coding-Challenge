64dc9379314efa2618c84157a60727d296f6fa2d
mdraselmiah95 <russelroy15@gmail.com>
2022-05-18 11:33:23 +0600

    JavaScript Data Attributes learnign

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -394,3 +394,16 @@ document.querySelector(".nav").addEventListener("click", function (e) {
   console.log("NAV", e.target, e.currentTarget);
 });
 
+// DOM Traversing
+// const h1 = document.querySelector("h1");
+// Going downwards: child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangeade";
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+h1.closest("h1").style.background = "var(--gradient-primary)";
