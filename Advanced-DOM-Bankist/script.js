f9ff0f2086b0455645264b3f3936f10f51486827
mdraselmiah95 <russelroy15@gmail.com>
2022-05-18 11:32:06 +0600

    JavaScript Data Attributes learnign

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -360,3 +360,14 @@ logo.classList.toggle("c");
 logo.classList.contains("c"); // not includes
 // Don't use
 logo.className = "jonas";
+
+// Types of Events and Event Handlers
+const h1 = document.querySelector("h1");
+const alertH1 = function (e) {
+  alert("addEventListener: Great! You are reading the heading :D");
+};
+h1.addEventListener("mouseenter", alertH1);
+setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
