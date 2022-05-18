f9cf83a8a3178bfcf1006a54e91e80778eaf0ee6
mdraselmiah95 <russelroy15@gmail.com>
2022-05-18 11:43:38 +0600

    JavaScript Prototypal Inheritance on Built-In Objects

--- Object_oriented_programming/script.js
+++ Object_oriented_programming/script.js
@@ -37,3 +37,20 @@ console.log(jonas.__proto__ === Person.prototype);
 console.log(Person.prototype.isPrototypeOf(jonas));
 console.log(Person.prototype.isPrototypeOf(matilda));
 console.log(Person.prototype.isPrototypeOf(Person));
+
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+console.dir(Person.prototype.constructor);
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+const h1 = document.querySelector("h1");
+console.dir((x) => x + 1);
