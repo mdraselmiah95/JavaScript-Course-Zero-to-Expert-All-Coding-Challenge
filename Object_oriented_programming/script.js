fc618f9076537bb921573fb2a04cdf745308af82
mdraselmiah95 <russelroy15@gmail.com>
2022-05-18 11:37:57 +0600

    JavaScript Constructor Functions and the new Operator

---
+++ Object_oriented_programming/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Constructor Functions and the new Operator
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  // Never to this!
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+console.log(jonas instanceof Person);
+Person.hey = function () {
+  console.log("Hey there 👋");
+  console.log(this);
+};
+Person.hey();
