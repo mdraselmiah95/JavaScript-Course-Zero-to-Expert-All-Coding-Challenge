d58d46ca066b83b91d02799a8c2b9f7a185ec488
mdraselmiah95 <russelroy15@gmail.com>
2022-05-18 11:39:32 +0600

    JavaScript Constructor Functions and the new Operator

--- Object_oriented_programming/script.js
+++ Object_oriented_programming/script.js
@@ -24,3 +24,16 @@ Person.hey = function () {
   console.log(this);
 };
 Person.hey();
+
+// Prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+jonas.calcAge();
+matilda.calcAge();
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
