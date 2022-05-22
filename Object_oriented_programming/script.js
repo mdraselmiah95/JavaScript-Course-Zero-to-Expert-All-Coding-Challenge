9612fc587a32a5a00542ba3a6a86c7dbd8b44895
mdraselmiah95 <russelroy15@gmail.com>
2022-05-22 18:38:05 +0600

    new Coding Challenge

--- Object_oriented_programming/script.js
+++ Object_oriented_programming/script.js
@@ -66,3 +66,22 @@ DATA CAR 1: 'BMW' going at 120 km/h
 DATA CAR 2: 'Mercedes' going at 95 km/h
 GOOD LUCK 😀
 */
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
