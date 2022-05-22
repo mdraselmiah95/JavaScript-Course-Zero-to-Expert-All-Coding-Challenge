793e63cf7c0c7a37b28184c7a9f687beb45a995b
mdraselmiah95 <russelroy15@gmail.com>
2022-05-22 19:06:04 +0600

    Setters and Getters

--- Object_oriented_programming/script.js
+++ Object_oriented_programming/script.js
@@ -141,3 +141,14 @@ const walter = new PersonCl("Walter White", 1965);
 // PersonCl.hey();
 
 // Setters and Getters
+
+const account = {
+  owner: "Jonas",
+  movements: [200, 530, 120, 300],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
