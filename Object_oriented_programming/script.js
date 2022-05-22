a04cf48c04f5ad51010ace4d4d58c647a7c7901e
mdraselmiah95 <russelroy15@gmail.com>
2022-05-22 19:36:33 +0600

    Inheritance Between Classes: Constructor Functions

--- Object_oriented_programming/script.js
+++ Object_oriented_programming/script.js
@@ -232,3 +232,9 @@ const Student = function (firstName, birthYear, course) {
   Person.call(this, firstName, birthYear);
   this.course = course;
 };
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
