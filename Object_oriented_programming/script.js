884f93f4883ade3e0f114f505324c469de184a19
mdraselmiah95 <russelroy15@gmail.com>
2022-05-25 00:08:27 +0600

    Coding Challenge #4

--- Object_oriented_programming/script.js
+++ Object_oriented_programming/script.js
@@ -404,3 +404,13 @@ Account.helper();
 // Chaining
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
+
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+GOOD LUCK 😀
+*/
