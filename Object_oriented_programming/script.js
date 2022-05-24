0cb306f270f9e1f632ce215f416f2c7fcb95cbb2
mdraselmiah95 <russelroy15@gmail.com>
2022-05-24 19:25:50 +0600

    Inheritance between classes : ES6

--- Object_oriented_programming/script.js
+++ Object_oriented_programming/script.js
@@ -400,3 +400,7 @@ Account.helper();
 // console.log(acc1.#movements);
 // console.log(acc1.#pin);
 // console.log(acc1.#approveLoan(100));
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
