fad0e056e93b82dba84cc4e7cb25d3bc1765b41d
mdraselmiah95 <russelroy15@gmail.com>
2022-05-25 17:43:43 +0600

    Coding Challenge #4

--- Object_oriented_programming/script.js
+++ Object_oriented_programming/script.js
@@ -436,3 +436,25 @@ class CarCl {
     this.speed = speed * 1.6;
   }
 }
+
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }`
+    );
+    return this;
+  }
+}
