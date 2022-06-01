b4756e76a8df3365c69b47c968aee99602d33ae0
mdraselmiah95 <russelroy15@gmail.com>
2022-06-01 22:44:01 +0600

    APPLICATION ARCHITECTURE

--- Mapty/script.js
+++ Mapty/script.js
@@ -61,3 +61,17 @@ class Cycling extends Workout {
     return this.speed;
   }
 }
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
+
+///////////////////////////////////////
+// APPLICATION ARCHITECTURE
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
