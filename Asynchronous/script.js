fe3355f17bbca4d7ea51632d7769f136d0bb3ccb
mdraselmiah95 <russelroy15@gmail.com>
2022-06-17 00:10:39 +0600

    JavaScript Callback Hell

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -110,3 +110,19 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+
+// Consuming Promises
+// Chaining Promises
+// Handling Rejected Promises
+// Throwing Errors Manually
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
