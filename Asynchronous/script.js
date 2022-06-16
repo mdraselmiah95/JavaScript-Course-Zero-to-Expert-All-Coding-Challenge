be3a075c6205b59b38f0e882fe736298d0db88bc
mdraselmiah95 <russelroy15@gmail.com>
2022-06-17 00:11:48 +0600

    JavaScript Callback Hell

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -158,3 +158,33 @@ setTimeout(() => {
 //       countriesContainer.style.opacity = 1;
 //     });
 // };
+
+const getCountryData = function (country) {
+  // Country 1
+  getJSON(
+    `https://restcountries.eu/rest/v2/name/${country}`,
+    "Country not found"
+  )
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) throw new Error("No neighbour found!");
+      // Country 2
+      return getJSON(
+        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+        "Country not found"
+      );
+    })
+    .then((data) => renderCountry(data, "neighbour"))
+    .catch((err) => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+btn.addEventListener("click", function () {
+  getCountryData("portugal");
+});
+// getCountryData('australia');
