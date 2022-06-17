23a3bea232d6edb3c182b546083b8b2365c1d08a
mdraselmiah95 <russelroy15@gmail.com>
2022-06-18 00:04:21 +0600

    Building a Simple Promise

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -209,3 +209,18 @@ Promise.resolve("Resolved promise 2").then((res) => {
   console.log(res);
 });
 console.log("Test end");
+
+// Building a Simple Promise
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log("Lottery draw is happening 🔮");
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve("You WIN 💰");
+    } else {
+      reject(new Error("You lost your money 💩"));
+    }
+  }, 2000);
+});
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
