077ee12cf0de121f0a414c58ecc70dcece6d46b6
mdraselmiah95 <russelroy15@gmail.com>
2022-06-20 22:54:22 +0600

    Modern-JS-Modules-Tooling

---
+++ Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,16 @@
+// Exporting and Importing in ES6 Modules
+
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log("Importing module");
+// console.log(shippingCost);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
