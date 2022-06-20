5ff5d3f2e7d75a4be9cfc865a0505d0557d1f3c2
mdraselmiah95 <russelroy15@gmail.com>
2022-06-20 22:58:00 +0600

    The Module Pattern

---
+++ Modern-JS-Modules-Tooling/shoppingCart.js
@@ -0,0 +1,7 @@
+// Exporting module
+console.log("Exporting module");
+
+// Blocking code
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
