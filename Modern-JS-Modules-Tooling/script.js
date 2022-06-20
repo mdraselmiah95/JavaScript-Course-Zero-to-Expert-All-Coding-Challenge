96cf798fa74c4ae75229aef475450f1d297b2988
mdraselmiah95 <russelroy15@gmail.com>
2022-06-20 22:56:16 +0600

    The Module Pattern

--- Modern-JS-Modules-Tooling/script.js
+++ Modern-JS-Modules-Tooling/script.js
@@ -39,3 +39,30 @@ console.log(lastPost);
 // lastPost.then(last => console.log(last));
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+// The Module Pattern
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+    );
+  };
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+ShoppingCart2.addToCart("apple", 4);
+ShoppingCart2.addToCart("pizza", 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
