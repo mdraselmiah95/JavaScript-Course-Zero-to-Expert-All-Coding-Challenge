ba44c47b78dae2371fba614c7071a8abe2ea0026
mdraselmiah95 <russelroy15@gmail.com>
2022-06-20 22:55:22 +0600

    Modern-JS-Modules-Tooling

--- Modern-JS-Modules-Tooling/script.js
+++ Modern-JS-Modules-Tooling/script.js
@@ -14,3 +14,28 @@ console.log("Importing module");
 
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // console.log(price);
+
+import add, { cart } from "./shoppingCart.js";
+add("pizza", 2);
+add("bread", 5);
+add("apples", 4);
+
+console.log(cart);
+
+// Top-Level Await (ES2022)
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost);
+// Not very clean
+// lastPost.then(last => console.log(last));
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
