bff4e1c696118d9435b28b1f22f821635ac7cf0d
mdraselmiah95 <russelroy15@gmail.com>
2022-06-22 20:42:07 +0600

    Introduction to NPM

--- Modern-JS-Modules-Tooling/script.js
+++ Modern-JS-Modules-Tooling/script.js
@@ -114,3 +114,15 @@ class Person {
   }
 }
 const jonas = new Person("Jonas");
+
+console.log("Jonas" ?? null);
+
+console.log(cart.find((el) => el.quantity >= 2));
+Promise.resolve("TEST").then((x) => console.log(x));
+
+import "core-js/stable";
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// Polifilling async functions
+import "regenerator-runtime/runtime";
