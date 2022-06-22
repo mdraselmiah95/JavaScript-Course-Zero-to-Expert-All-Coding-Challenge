6e668616256cdc9bb6b2de6970b9b5ad23b759a3
mdraselmiah95 <russelroy15@gmail.com>
2022-06-22 20:23:46 +0600

    Introduction to NPM

--- Modern-JS-Modules-Tooling/script.js
+++ Modern-JS-Modules-Tooling/script.js
@@ -96,3 +96,21 @@ const state = {
 };
 const stateClone = Object.assign({}, state);
 const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  #greeting = "Hey";
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+const jonas = new Person("Jonas");
