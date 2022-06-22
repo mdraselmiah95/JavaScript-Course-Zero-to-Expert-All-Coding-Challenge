a32ae0d23032db9416e4f193ba360ed6ca635214
mdraselmiah95 <russelroy15@gmail.com>
2022-06-22 20:56:22 +0600

    Clean Code

---
+++ Modern-JS-Modules-Tooling/clean.js
@@ -0,0 +1,18 @@
+"strict mode";
+
+const budget = Object.freeze([
+  { value: 250, description: "Sold old TV 📺", user: "jonas" },
+  { value: -45, description: "Groceries 🥑", user: "jonas" },
+  { value: 3500, description: "Monthly salary 👩‍💻", user: "jonas" },
+  { value: 300, description: "Freelancing 👩‍💻", user: "jonas" },
+  { value: -1100, description: "New iPhone 📱", user: "jonas" },
+  { value: -20, description: "Candy 🍭", user: "matilda" },
+  { value: -125, description: "Toys 🚂", user: "matilda" },
+  { value: -1800, description: "New Laptop 💻", user: "jonas" },
+]);
+
+const spendingLimits = Object.freeze({
+  jonas: 1500,
+  matilda: 100,
+});
+// spendingLimits.jay = 200;
