bf8aa47a7dd1ce74afa8776b5763aca70c802145
mdraselmiah95 <russelroy15@gmail.com>
2022-11-03 02:33:33 +0600

    new code added

--- JS-All-You-Need-to-Know/string.js
+++ JS-All-You-Need-to-Know/string.js
@@ -5,3 +5,7 @@ console.log(a > b);
 console.log(a < b);
 console.log("a" > "A");
 console.log("A" > "a");
+
+// Escape Notation
+const str = "This is the most important 'TOPIC' in JavaScript.";
+console.log(str);
