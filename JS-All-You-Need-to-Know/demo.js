da09a78893325040538d771c88a33f7a09a4ee94
mdraselmiah95 <russelroy15@gmail.com>
2022-11-02 19:27:22 +0600

    JavaScript String

--- JS-All-You-Need-to-Know/demo.js
+++
@@ -1,5 +0,0 @@
-const news = "this is cool";
-console.log(news);
-const n1 = 22;
-const n2 = 33;
-console.log(n1 + n2);
---
+++ JS-All-You-Need-to-Know/string.js
@@ -0,0 +1,7 @@
+var a = "abc";
+var b = "bcd";
+console.log(a === b);
+console.log(a > b);
+console.log(a < b);
+console.log("a" > "A");
+console.log("A" > "a");
