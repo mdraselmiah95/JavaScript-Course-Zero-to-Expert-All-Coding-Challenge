7742ce3fcaebc8e6ff5874efbf8c497e8f99b999
mdraselmiah95 <russelroy15@gmail.com>
2022-11-04 03:44:19 +0600

    Javascript Play with traverse

--- JS-All-You-Need-to-Know/string.js
+++ JS-All-You-Need-to-Know/string.js
@@ -26,3 +26,15 @@ for (var i = 0; i < arr.length; i++) {
   sum += arr[i];
 }
 console.log(sum);
+
+for (var i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) {
+    console.log(arr[i]);
+  }
+}
+
+for (var i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 !== 0) {
+    console.log(arr[i]);
+  }
+}
