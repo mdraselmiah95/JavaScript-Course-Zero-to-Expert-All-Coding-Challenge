3abc0df6dedc64d2e3999164a91a4a7a1ebb15a2
mdraselmiah95 <russelroy15@gmail.com>
2022-11-04 05:06:01 +0600

    Javascript multi Dimensional Array

---
+++ JS-All-You-Need-to-Know/play-with-array/multiDimensionalArray.js
@@ -0,0 +1,9 @@
+// Multi Dimensional Array
+
+var arr = [
+  [78, 88, 92, 82, 80],
+  [80, 81, 92, 88, 87],
+  [77, 83, 85, 90, 95],
+];
+console.log(arr[0][2]);
+console.log(arr[1]);
--- JS-All-You-Need-to-Know/play-with-array/search.js
+++ JS-All-You-Need-to-Know/play-with-array/search.js
@@ -1,8 +1,8 @@
 //Search Data
 
-var arr = [1, 3, 5, 6, 7, 8, 9, 11, 24, 68, 23, 27, 876];
+var arr = [1, 2, 3, 5, 6, 7, 8, 9, 11, 24, 68, 23, 27, 12, 34, 86];
 
-var find = 284;
+var find = 12;
 var isFound = false;
 
 for (var i = 0; i < arr.length; i++) {
@@ -14,5 +14,5 @@ for (var i = 0; i < arr.length; i++) {
 }
 
 if (!isFound) {
-  console.log("Data not Found 😲");
+  console.log("Data not Found 🔥");
 }
