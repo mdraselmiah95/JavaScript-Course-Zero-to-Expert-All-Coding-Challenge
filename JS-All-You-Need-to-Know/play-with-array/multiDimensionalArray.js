d265f52b986784cd37075c740831f5e7454c31e1
mdraselmiah95 <russelroy15@gmail.com>
2022-11-04 05:12:43 +0600

    Javascript multi Dimensional Array

--- JS-All-You-Need-to-Know/play-with-array/multiDimensionalArray.js
+++ JS-All-You-Need-to-Know/play-with-array/multiDimensionalArray.js
@@ -4,6 +4,14 @@ var arr = [
   [78, 88, 92, 82, 80],
   [80, 81, 92, 88, 87],
   [77, 83, 85, 90, 95],
+  [78, 69, 95, 76, 93],
 ];
-console.log(arr[0][2]);
-console.log(arr[1]);
+// console.log(arr[0][2]);
+// console.log(arr[1]);
+
+for (var i = 0; i < arr.length; i++) {
+  for (var j = 0; j < arr[i].length; j++) {
+    // console.log(arr[i][j]);
+    console.log("Element" + i + " : " + arr[i][j]);
+  }
+}
