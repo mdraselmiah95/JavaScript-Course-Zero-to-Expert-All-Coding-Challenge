2f8605ad41fb6f886d2cc00ba374a75f6bf552fa
mdraselmiah95 <russelroy15@gmail.com>
2022-11-04 05:20:49 +0600

    Javascript Reverse Arrayy

---
+++ JS-All-You-Need-to-Know/play-with-array/reverse.js
@@ -0,0 +1,11 @@
+// Reverse an Array
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (var i = 0; i < arr.length / 2; i++) {
+  var temp = arr[i];
+  arr[i] = arr[arr.length - 1 - i];
+  arr[arr.length - 1 - i] = temp;
+}
+
+console.log(arr);
