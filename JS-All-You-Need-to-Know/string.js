4f0f2926d42d8ce5c0e97490578843948c356a80
mdraselmiah95 <russelroy15@gmail.com>
2022-11-04 03:37:50 +0600

    Javascript Play with traverse

--- JS-All-You-Need-to-Know/string.js
+++ JS-All-You-Need-to-Know/string.js
@@ -16,5 +16,7 @@ var arr = [1, 3, 4, 5, 6, 8, 9, 20, 36, 88];
 console.log(arr.length - 1);
 
 for (var i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+  //   console.log(arr[i]);
+  arr[i] = arr[i] + 2;
 }
+console.log(arr);
