d6182bfc4c0b2d6fdb42c8dedabd5cf5a376752d
mdraselmiah95 <russelroy15@gmail.com>
2022-11-04 06:00:17 +0600

    Javascript Method

--- JS-All-You-Need-to-Know/play-with-array/method.js
+++ JS-All-You-Need-to-Know/play-with-array/method.js
@@ -1,4 +1,16 @@
 // Javascript Method 📝
 
-var arr = [4, 5, 6, 7, 8, 9];
-console.log(arr);
+var arr = [4, 5, 6, 7, 8, 9, 1, 3];
+var arr2 = [6, 8, 10, 33, 56, 77, 23, 56, 99];
+
+//join method
+// console.log(arr.join("|"));
+
+// arr.fill(false); Fill method
+// console.log(arr);
+
+const newArr = arr.concat(arr2); //concat method
+// console.log(newArr);
+
+const isArr = Array.isArray();
+// console.log(isArr);
