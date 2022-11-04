0370c8db5f7073cf2be735ef9153a6ef709a928f
mdraselmiah95 <russelroy15@gmail.com>
2022-11-04 06:31:02 +0600

    Javascript Method

--- JS-All-You-Need-to-Know/play-with-object/object.js
+++ JS-All-You-Need-to-Know/play-with-object/object.js
@@ -10,3 +10,9 @@ console.log(student);
 
 var stu = Object({ math: 88, chemistry: 75, physics: 79, higherMath: 95 });
 console.log(stu);
+
+//console.log(Object(), Array());
+
+var obj = {};
+obj.x = 5;
+console.log(obj);
