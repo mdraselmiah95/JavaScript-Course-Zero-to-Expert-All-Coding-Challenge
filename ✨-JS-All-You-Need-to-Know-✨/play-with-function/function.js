c8f15aea4af3a20a8c7d8abbc42bebce6a60e790
mdraselmiah95 <russelroy15@gmail.com>
2022-11-05 22:45:08 +0600

    Javascript Function

--- ✨-JS-All-You-Need-to-Know-✨/play-with-function/function.js
+++ ✨-JS-All-You-Need-to-Know-✨/play-with-function/function.js
@@ -2,5 +2,16 @@
 
 //What is Function => Input => Processing => Output
 
-var date = new Date().toLocaleDateString();
-console.log(date);
+function sum() {
+  var a = 10;
+  var b = 20;
+  console.log(a + b);
+}
+
+function sub() {
+  var a = 10;
+  var b = 20;
+  console.log(a - b);
+}
+sub();
+sum();
