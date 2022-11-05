ef411b761c22d9077afcf9a3820e85671dbd281f
mdraselmiah95 <russelroy15@gmail.com>
2022-11-05 21:53:31 +0600

    Javascript Object

--- ✨-JS-All-You-Need-to-Know-✨/play-with-object/compare.js
+++ ✨-JS-All-You-Need-to-Know-✨/play-with-object/compare.js
@@ -9,4 +9,11 @@ var obj2 = {
   b: 20,
 };
 
-console.log(obj1 === obj2);
+if (obj1.a === obj2.a && obj1.b === obj2.b) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+console.log(JSON.stringify(obj1) === JSON.stringify(obj2));
+//console.log(obj1 === obj2);
