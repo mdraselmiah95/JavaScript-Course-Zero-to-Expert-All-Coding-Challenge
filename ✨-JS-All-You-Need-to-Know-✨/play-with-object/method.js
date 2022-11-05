761c3c90c1e3d0fe8b17bd9cce9b46718acc8b47
mdraselmiah95 <russelroy15@gmail.com>
2022-11-05 22:22:15 +0600

    Javascript Object iterate

---
+++ ✨-JS-All-You-Need-to-Know-✨/play-with-object/method.js
@@ -0,0 +1,16 @@
+// Object Method
+
+var obj = {
+  x: 30,
+  y: 40,
+  z: 50,
+};
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+
+var obj2 = Object.assign({}, obj);
+obj2.x = 100;
+
+console.log(obj);
+console.log(obj2);
