38dad43941d668081a523a8e83190407710b0e16
mdraselmiah95 <russelroy15@gmail.com>
2022-11-07 01:31:40 +0600

    Javascript INNER function

--- ✨-JS-All-You-Need-to-Know-✨/play-with-function/inner.js
+++ ✨-JS-All-You-Need-to-Know-✨/play-with-function/inner.js
@@ -8,3 +8,17 @@ function something(greet, name) {
 }
 
 something("Good Day", "Rasel");
+
+function something(greet, name) {
+  function getFirstName() {
+    if (name) {
+      return name.split(" ")[1]; //[0]=first name
+    } else {
+      return "";
+    }
+  }
+  var message = greet + " " + getFirstName();
+  console.log(message);
+}
+
+something("Good night", "Rasel miah");
