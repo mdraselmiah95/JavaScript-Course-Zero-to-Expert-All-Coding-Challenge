40254becddf69a29dfe7964ba8950c5bad04b19c
mdraselmiah95 <russelroy15@gmail.com>
2022-11-06 22:59:29 +0600

    Javascript function return

--- ✨-JS-All-You-Need-to-Know-✨/play-with-function/return.js
+++ ✨-JS-All-You-Need-to-Know-✨/play-with-function/return.js
@@ -3,8 +3,19 @@ function addAll() {
   for (var i = 0; i < arguments.length; i++) {
     sum += arguments[i];
   }
+
   return sum;
 }
 
 var result = addAll(1, 2, 4, 5);
 console.log(result);
+
+const cool = (a, b, c) => {
+  const sum = a + b;
+  const total = sum / c;
+  const result = total * 2;
+  return result;
+};
+
+const result2 = cool(20, 60, 2);
+console.log(result2);
