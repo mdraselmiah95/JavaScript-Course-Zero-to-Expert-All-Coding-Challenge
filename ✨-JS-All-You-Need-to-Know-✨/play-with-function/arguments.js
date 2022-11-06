661bf2d841c746cff6597b5952b5451d7af12f38
mdraselmiah95 <russelroy15@gmail.com>
2022-11-06 20:19:33 +0600

    Javascript function arguments

--- ✨-JS-All-You-Need-to-Know-✨/play-with-function/arguments.js
+++ ✨-JS-All-You-Need-to-Know-✨/play-with-function/arguments.js
@@ -5,4 +5,33 @@ function add(a, b) {
   console.log(result);
 }
 
-add(10, 55);
+function sub(a, b) {
+  var result = a - b;
+  console.log(result);
+}
+
+// add(10, 55);
+// add(7, 3);
+// sub(10, 3);
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [7, 8, 9];
+
+var sum1 = 0;
+for (var i = 0; i < arr1.length; i++) {
+  sum1 += arr1[i];
+}
+console.log(sum1);
+
+var sum2 = 0;
+for (var i = 0; i < arr2.length; i++) {
+  sum2 += arr2[i];
+}
+console.log(sum2);
+
+var sum3 = 0;
+for (var i = 0; i < arr3.length; i++) {
+  sum3 += arr3[i];
+}
+console.log(sum3);
