7977e6da2511237b7d6920d656427b6e5ec08c78
mdraselmiah95 <russelroy15@gmail.com>
2022-11-07 22:08:04 +0600

    WIP in functional programming in javascript forEach

--- AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/forEach.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/forEach.js
@@ -2,9 +2,24 @@
 
 var arr = [1, 3, 4, 5, 6];
 
+/*
 var sum = 0;
 arr.forEach(function (value, index, arr) {
   //   console.log(value, index, arr);
   sum += value;
 });
+console.log(sum);
+*/
+
+function forEach(arr, cb) {
+  for (var i = 0; i < arr.length; i++) {
+    cb(arr[i]);
+  }
+}
+
+var sum = 0;
+forEach(arr, function (value) {
+  sum += value;
+});
+
 console.log(sum);
