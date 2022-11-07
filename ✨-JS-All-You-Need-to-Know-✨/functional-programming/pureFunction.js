6f0b8848a7ef4fe048d864dd3b46a45abd14da4c
mdraselmiah95 <russelroy15@gmail.com>
2022-11-07 19:32:39 +0600

    WIP in functional programming in javascript pure function

--- ✨-JS-All-You-Need-to-Know-✨/functional-programming/pureFunction.js
+++ ✨-JS-All-You-Need-to-Know-✨/functional-programming/pureFunction.js
@@ -4,3 +4,10 @@
  * It Return the same result if the given the same arguments
  * it does not cause any observable side effects
  */
+
+function sqr(n) {
+  return n * n;
+}
+console.log(sqr(3));
+console.log(sqr(4));
+console.log(sqr(7));
