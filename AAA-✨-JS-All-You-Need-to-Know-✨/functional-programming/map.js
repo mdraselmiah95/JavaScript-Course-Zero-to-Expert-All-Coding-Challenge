c410b9bee3a27e8752a8233b306ac87252573bec
mdraselmiah95 <russelroy15@gmail.com>
2022-11-08 01:41:59 +0600

    WIP in functional programming in javascript Map

--- AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/map.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/map.js
@@ -11,13 +11,25 @@ var sqrArr = arr.map(function (value) {
 // console.log(arr);
 // console.log(sqrArr);
 
-function myMap(arr) {
+function myMap(arr, cb) {
   var newArr = [];
   for (var i = 0; i < arr.length; i++) {
-    var temp = arr[i] * arr[i];
+    // var temp = arr[i] * arr[i];
+    var temp = cb(arr[i], i, arr);
     newArr.push(temp);
   }
   return newArr;
 }
 
-console.log(myMap(arr));
+// console.log(myMap(arr));
+var qb = myMap(arr, function (value) {
+  return value * value * value;
+});
+
+var sqr = myMap(arr, function (value) {
+  return value * value;
+});
+
+console.log(arr);
+console.log(qb);
+console.log(sqr);
