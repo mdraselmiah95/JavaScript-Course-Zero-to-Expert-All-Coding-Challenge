f84deb645481d2a0d561c99a855ef012dff8002d
mdraselmiah95 <russelroy15@gmail.com>
2022-11-07 19:58:24 +0600

    WIP in functional programming in javascript first class function

--- AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/first-class-function.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/first-class-function.js
@@ -1,1 +1,5 @@
 // First class function in Javascript. ✊
+
+/*
+A programming language is said to have First-class functions when functions in that language are treated like any other variable. For example, in such a language, a function can be passed as an argument to other functions, can be returned by another function and can be assigned as a value to a variable.
+*/
