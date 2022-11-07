53a5de5e4779b99b3c60d2283a7b07d2932bdc43
mdraselmiah95 <russelroy15@gmail.com>
2022-11-08 02:53:15 +0600

    Learning JavaScript Reduce mathod

--- AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/reduce.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/reduce.js
@@ -1,6 +1,6 @@
 // Javascript Reduce method 🉑
 
-var arr = [1, 2, 3, 9, 4, 5, 6, 7, 8];
+var arr = [75, 1, 2, 3, 9, 4, 5, 6, 7, 8];
 
 var sum = arr.reduce(function (prev, curr) {
   return prev + curr;
@@ -10,5 +10,38 @@ var max = arr.reduce(function (prev, curr) {
   return Math.max(prev, curr);
 }, 0);
 
-console.log(sum);
-console.log(max);
+// console.log(sum);
+// console.log(max);
+
+function myReduce(arr, cb, acc) {
+  for (var i = 0; i < arr.length; i++) {
+    acc = cb(acc, arr[i]);
+  }
+  return acc;
+}
+
+var sum2 = myReduce(
+  arr,
+  function (prev, curr) {
+    return prev + curr;
+  },
+  0
+);
+
+var max2 = myReduce(
+  arr,
+  function (prev, curr) {
+    return Math.max(prev, curr);
+  },
+  0
+);
+
+var min = myReduce(
+  arr,
+  function (prev, curr) {
+    return Math.min(prev, curr);
+  },
+  arr[0]
+);
+
+console.log(sum2, max2, min);
