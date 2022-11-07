d8c26e58d73fdcc17b6bcc66d1c1893b6c462e57
mdraselmiah95 <russelroy15@gmail.com>
2022-11-08 03:06:21 +0600

    Learning JavaScript sort mathod

---
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/sort.js
@@ -0,0 +1,22 @@
+// Javascript sort method 🗽
+
+var persons = [
+  {
+    name: "A",
+    age: 24,
+  },
+  {
+    name: "B",
+    age: 18,
+  },
+  {
+    name: "C",
+    age: 27,
+  },
+  {
+    name: "D",
+    age: 21,
+  },
+];
+
+var arr = [4, 75, 1, 2, 3, 9, 4, 5, 6, 7, 8, 11, 19];
