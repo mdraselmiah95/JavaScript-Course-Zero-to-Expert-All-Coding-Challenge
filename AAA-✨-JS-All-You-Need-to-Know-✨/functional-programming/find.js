314b7a3f4ac0f4755c65036e556bbbd1547ddb0d
mdraselmiah95 <russelroy15@gmail.com>
2022-11-08 03:01:53 +0600

    Learning JavaScript find mathod

--- AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/find.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/find.js
@@ -10,4 +10,19 @@ var result2 = arr.findIndex(function (value) {
   return value === 9;
 });
 
-console.log(result, result2);
+// console.log(result, result2);
+
+function myFind(arr, cb) {
+  for (var i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) {
+      return i;
+      //   return arr[i];
+    }
+  }
+}
+
+var result3 = myFind(arr, function (value) {
+  return value === 9;
+});
+
+console.log(result3);
