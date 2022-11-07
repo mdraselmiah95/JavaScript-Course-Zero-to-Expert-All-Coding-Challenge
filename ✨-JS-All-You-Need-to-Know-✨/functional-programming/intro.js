bc5dfbefbc30fdd3f89074cb5c88a3e35201cd01
mdraselmiah95 <russelroy15@gmail.com>
2022-11-07 19:25:26 +0600

    functional programming in javascript

---
+++ ✨-JS-All-You-Need-to-Know-✨/functional-programming/intro.js
@@ -0,0 +1,10 @@
+// Functional Programming in Javascript
+
+/*
+Three main Terms:
+    1. Pure Function
+    2. First Class Function
+    3. Higher Order Function
+*/
+
+// ***************** Function Acts like a Value **************
