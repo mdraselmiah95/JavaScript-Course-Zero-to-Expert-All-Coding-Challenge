5e58c9db7243a333a0134e9bca4dc23858013632
mdraselmiah95 <russelroy15@gmail.com>
2022-11-07 21:11:35 +0600

    WIP in functional programming in javascript callback function

---
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/Callback.js
@@ -0,0 +1,26 @@
+// Callback function
+
+function sample(a, b, cb) {
+  var c = a + b;
+  var d = a - b;
+
+  var result = cb(c, d);
+  return result;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+var result = sample(13, 7, sum);
+console.log(result);
+
+var result2 = sample(16, 4, function (c, d) {
+  return c - d;
+});
+console.log(result2);
+
+var result3 = sample(6, 4, function (c, d) {
+  return c * d;
+});
+console.log(result3);
