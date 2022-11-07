93e25f693c9edd942a325a825acb170dbf4ca246
mdraselmiah95 <russelroy15@gmail.com>
2022-11-08 02:32:01 +0600

    Learning JavaScript in filter

--- AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/filter.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/filter.js
@@ -6,4 +6,16 @@ var filteredArr = arr.filter(function (value) {
   return value % 2 === 1;
 });
 
-console.log(filteredArr);
+// console.log(filteredArr);
+
+function myFilter(arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(myFilter(arr));
