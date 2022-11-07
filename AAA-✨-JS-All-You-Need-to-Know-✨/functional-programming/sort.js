f79a585ab3fc41c892698165db44f48f17db2a12
mdraselmiah95 <russelroy15@gmail.com>
2022-11-08 03:12:01 +0600

    Learning JavaScript sort mathod

--- AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/sort.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/sort.js
@@ -19,4 +19,22 @@ var persons = [
   },
 ];
 
-var arr = [4, 75, 1, 2, 3, 9, 4, 5, 6, 7, 8, 11, 19];
+var arr = [4, -2, 75, -8, 1, 2, 3, 9, 4, 5, 6, 7, 8, 11, 19];
+
+arr.sort();
+// console.log(arr);
+
+persons.sort();
+// console.log(persons);
+
+arr.sort(function (a, b) {
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else {
+    return 0;
+  }
+});
+
+console.log(arr);
