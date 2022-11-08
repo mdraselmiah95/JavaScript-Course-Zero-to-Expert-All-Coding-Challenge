f3a0e6102fe81d97177f11a6c54d1284af773d64
mdraselmiah95 <russelroy15@gmail.com>
2022-11-08 19:05:02 +0600

    Learning Javascript return

--- AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/return.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/return.js
@@ -8,6 +8,26 @@ function greet(msg) {
 }
 
 var gm = greet("Good Evening");
+var gn = greet("Good Night");
+var hello = greet("Hello");
 // console.log(typeof gm);
 var msg = gm("Rasel mia");
-console.log(msg);
+var mas2 = gn("Dev Rasel");
+// console.log(msg, mas2);
+
+function base(b) {
+  return function (n) {
+    var result = 1;
+    for (var i = 0; i < b; i++) {
+      result *= n;
+    }
+    return result;
+  };
+}
+
+var base10 = base(10);
+var base5 = base(2);
+
+console.log(base10(2), base5(2));
+console.log(base5(3));
+console.log(base5(5));
