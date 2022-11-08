c0229a1b086d4064d8fe68d91e76f09bdc6998a2
mdraselmiah95 <russelroy15@gmail.com>
2022-11-08 21:28:14 +0600

    Learning Javascript Composition

---
+++ AAA-✨-JS-All-You-Need-to-Know-✨/functional-programming/composition.js
@@ -0,0 +1,15 @@
+// Function Composition 🉑
+
+function print(inp) {
+  console.log(inp);
+}
+
+function multiplyByFive(n) {
+  return n * 5;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+print(multiplyByFive(add(8, 2)));
