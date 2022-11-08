e09bb68214ca7ba9c4eab726977929570e436389
mdraselmiah95 <russelroy15@gmail.com>
2022-11-09 01:59:00 +0600

    Learning javascript exection context hoisting

--- AAA-✨-JS-All-You-Need-to-Know-✨/scope-and-closure/function.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/scope-and-closure/function.js
@@ -4,10 +4,18 @@ function abc() {
   console.log("I am Function.");
 }
 
-abc();
+// newAbc();
+console.log(abc);
+
+var newAbc = function () {
+  console.log("I am new ABC function");
+};
+
+newAbc();
+console.log(typeof newAbc);
 
 // Creation phase:
 // abc = ref
+// newAbc = undefine
 
 // Execution Phase:
-//
