8b575a5121b8ad1be518e8b16ef27729dcba0b5b
mdraselmiah95 <russelroy15@gmail.com>
2022-11-09 21:57:31 +0600

    Learning Javascript scope

--- AAA-✨-JS-All-You-Need-to-Know-✨/scope-and-closure/scope.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/scope-and-closure/scope.js
@@ -1,10 +1,44 @@
-// Javascript scope
+// Javascript scope & Lexical scope ✡️
 
 var x = 50;
 
 function test() {
-  //   var x = 39;
+  var x = 39;
+
   console.log(x);
+
+  function nested() {
+    // var y=64
+    console.log(x);
+  }
+
+  // console.log(y);
+  nested();
 }
 
-test();
+// test();
+
+var a = 11;
+
+function A() {
+  var b = 12;
+  function B() {
+    var c = 23;
+    console.log(c);
+  }
+
+  function C() {
+    var d = 56;
+    console.log(d);
+  }
+  console.log(b);
+  D();
+  B();
+  C();
+}
+
+function D(n) {
+  return n + a;
+}
+
+A();
