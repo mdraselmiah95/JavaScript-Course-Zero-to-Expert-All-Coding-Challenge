58be25125d42db91cf6d9a504e3efa38570ca340
mdraselmiah95 <russelroy15@gmail.com>
2022-11-10 20:03:53 +0600

    Learning Javascript this keyword

--- AAA-✨-JS-All-You-Need-to-Know-✨/object-oriented-programming/index.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/object-oriented-programming/index.js
@@ -4,9 +4,21 @@ var rect = {
 
   draw: function () {
     console.log("I am react.");
+    this.printProperties();
+  },
+  printProperties: function () {
     console.log("My width is " + this.width);
     console.log("My height is " + this.height);
   },
 };
 
 rect.draw();
+// rect.height = 120;
+// rect.draw();
+
+// this keyword  in javascript
+
+function myFunc() {
+  console.log(this);
+}
+new myFunc();
