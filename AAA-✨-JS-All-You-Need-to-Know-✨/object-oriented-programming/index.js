b8bbd73270d2a3b4b868bd3eb9d61f863d0d55e1
mdraselmiah95 <russelroy15@gmail.com>
2022-11-10 20:19:54 +0600

    Learning Javascript this keyword

--- AAA-✨-JS-All-You-Need-to-Know-✨/object-oriented-programming/index.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/object-oriented-programming/index.js
@@ -1,3 +1,5 @@
+/*
+
 var rect = {
   width: 100,
   height: 50,
@@ -5,6 +7,7 @@ var rect = {
   draw: function () {
     console.log("I am react.");
     this.printProperties();
+    console.log(this);
   },
   printProperties: function () {
     console.log("My width is " + this.width);
@@ -12,13 +15,54 @@ var rect = {
   },
 };
 
-rect.draw();
+*/
+
+// rect.draw();
 // rect.height = 120;
 // rect.draw();
 
 // this keyword  in javascript
 
-function myFunc() {
-  console.log(this);
-}
-new myFunc();
+// function myFunc() {
+//   console.log(this);
+// }
+// new myFunc();
+
+/*
+
+var another = {
+  width: 87,
+  height: 29,
+
+  print: rect.printProperties,
+};
+
+*/
+
+// console.log(another.print);
+// another.print();
+
+// Factory pattern
+
+var createRect = function (width, height) {
+  return {
+    width: width,
+    height: height,
+
+    draw: function () {
+      console.log("I am react.");
+      this.printProperties();
+      console.log(this);
+    },
+    printProperties: function () {
+      console.log("My width is " + this.width);
+      console.log("My height is " + this.height);
+    },
+  };
+};
+
+var rect1 = createRect(100, 150);
+rect1.draw();
+
+var rect2 = createRect(110, 120);
+rect2.draw();
