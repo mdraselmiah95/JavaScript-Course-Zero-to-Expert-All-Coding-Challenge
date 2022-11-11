ff0e1c826e45a8c110db596ce7122c4b2e4c41e6
mdraselmiah95 <russelroy15@gmail.com>
2022-11-12 02:19:50 +0600

    Learning Javascript Prototype and prototypal inheritance in javascript

--- AAA-✨-JS-All-You-Need-to-Know-✨/prototype-and-prototypal-inheritance/index.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/prototype-and-prototypal-inheritance/index.js
@@ -3,4 +3,21 @@
 var obj = {};
 var obj2 = new Object();
 
-console.log(Object.getPrototypeOf(obj), Object.getPrototypeOf(obj2));
+// console.log(Object.getPrototypeOf(obj), Object.getPrototypeOf(obj2));
+//arr object
+
+function Person(name) {
+  this.name = name;
+}
+
+var p1 = new Person("Rasel miah");
+// console.log(p1);
+
+var person = {
+  name: "MD Rasel",
+};
+// console.log(person);
+
+for (var i in person) {
+  console.log(i);
+}
