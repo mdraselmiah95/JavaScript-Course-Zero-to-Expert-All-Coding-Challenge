3bd9501660ff27058441efb90f9f1a32b021e624
mdraselmiah95 <russelroy15@gmail.com>
2022-11-11 16:55:13 +0600

    Learning Javascript deeply constructor

--- AAA-✨-JS-All-You-Need-to-Know-✨/object-oriented-programming/index.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/object-oriented-programming/index.js
@@ -85,7 +85,21 @@ var Rectangle = function (width, height) {
   };
 };
 
-var rec3 = new Rectangle(22, 98);
-rec3.draw();
-console.log(rec3());
+// var rec3 = new Rectangle(22, 98);
+// rec3.draw();
+// console.log(rec3());
+
+// New keyword in javascript 🆕
+
+function MyNew(constructor) {
+  var obj = {};
+  Object.setPrototypeOf(obj, constructor.prototype);
+  var argsArray = Array.prototype.slice.apply(arguments);
+  constructor.apply(obj, argsArray.slice(1));
+
+  return obj;
+}
+
+var rec4 = MyNew(Rectangle, 45, 30);
+rec4.draw();
 // Constructor Function 🥇
