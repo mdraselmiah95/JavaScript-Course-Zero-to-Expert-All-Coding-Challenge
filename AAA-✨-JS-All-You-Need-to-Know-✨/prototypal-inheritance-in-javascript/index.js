12690229dd03494c0a1cac05a2993195ba6fd2a6
mdraselmiah95 <russelroy15@gmail.com>
2022-11-13 17:43:31 +0600

    Learning Javascript Prototype and prototypal inheritance

--- AAA-✨-JS-All-You-Need-to-Know-✨/prototypal-inheritance-in-javascript/index.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/prototypal-inheritance-in-javascript/index.js
@@ -1,15 +1,29 @@
-function Square(width) {
-  this.width = width;
-}
+function Shape() {}
 
-Square.prototype = {
-  draw: function () {
-    console.log("Drawing");
-  },
+Shape.prototype = {
   common: function () {
     console.log("I am common Method");
   },
 };
 
-var sqe = new Square(12);
-console.log(sqe);
+function Square(width) {
+  this.width = width;
+}
+
+Square.prototype = Object.create(Shape.prototype);
+
+Square.prototype = {
+  draw: function () {
+    console.log("Drawing");
+  },
+};
+
+var shape = new Shape();
+var sqr = new Square(6);
+
+// shape => shape => object
+// sqr => Square => Object
+// sqr => Square => Shape => Object
+
+// var sqe = new Square(12);
+// console.log(sqe);
