6ad3c3efcadadf808e3c7329f09c665403c229d3
mdraselmiah95 <russelroy15@gmail.com>
2022-11-16 18:53:01 +0600

    Learning Javascript ES6

--- AAA-✨-JS-All-You-Need-to-Know-✨/Modern-JavaScript-es6-C2/src/scripts/app.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Modern-JavaScript-es6-C2/src/scripts/app.js
@@ -3,6 +3,23 @@ import "../styles/index.scss";
 // Iterator Basic 🥇
 
 const arr = [1, 2, 3, 4, 5, 6];
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+function createIterator(collection) {
+  let i = 0;
+  return {
+    next() {
+      return {
+        done: i >= collection.length,
+        value: collection[i++],
+      };
+    },
+  };
 }
+
+const iterator = createIterator(arr);
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
