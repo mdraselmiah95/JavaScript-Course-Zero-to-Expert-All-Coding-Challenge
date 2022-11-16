10e1ba859ea8b4206cb75006f83831c3cceb3361
mdraselmiah95 <russelroy15@gmail.com>
2022-11-16 19:32:57 +0600

    Learning Javascript ES6

--- AAA-✨-JS-All-You-Need-to-Know-✨/Modern-JavaScript-es6-C2/src/scripts/app.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Modern-JavaScript-es6-C2/src/scripts/app.js
@@ -72,4 +72,20 @@ for (let v of obj) {
 
 */
 
-function generate() {}
+// function* generate() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+
+function* generate(collection) {
+  for (let i = 0; i < collection.length; i++) {
+    yield collection[i];
+  }
+}
+
+let it = generate(arr);
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
