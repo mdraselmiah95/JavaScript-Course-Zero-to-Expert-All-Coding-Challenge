7ca3665bd37f4caaa048469f07da44f751d298e6
mdraselmiah95 <russelroy15@gmail.com>
2022-11-18 20:28:59 +0600

    Learning Javascript Error handling

--- AAA-✨-JS-All-You-Need-to-Know-✨/Error-in-Javascript/src/scripts/app.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Error-in-Javascript/src/scripts/app.js
@@ -2,8 +2,16 @@ import "../styles/index.scss";
 
 // Error in Javascript 🏸
 
-let n = 10;
+// let n = 10;
 
-if (n > 5) {
-  throw new Error(" N is greater in 5");
+// if (n > 5) {
+//   throw new Error(" N is greater in 5");
+// }
+
+//console.log(Number.MAX_VALUE);
+
+let n = 58;
+
+if (n > 50) {
+  throw new RangeError("The number is too long.. 💣");
 }
