35f8ac93c6d550ea2016dad1f85c68afc8b9cca4
mdraselmiah95 <russelroy15@gmail.com>
2022-11-19 01:13:57 +0600

    Learning Javascript Asynchronous Programming is Action

--- AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson2.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson2.js
@@ -1,1 +1,9 @@
-console.log("this is cool.");
+//  Asynchronous Programming is Action 🔋
+
+console.log("I am line 1");
+console.log("I am line 2");
+console.log("I am line 3");
+
+setTimeout(() => {
+  console.log("I have done.");
+}, 3000);
