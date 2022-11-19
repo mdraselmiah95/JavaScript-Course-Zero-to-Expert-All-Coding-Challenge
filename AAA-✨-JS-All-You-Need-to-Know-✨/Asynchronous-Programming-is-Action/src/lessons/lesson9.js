ae9c4754d409fb7682ccf054d92b89332bf8cca7
mdraselmiah95 <russelroy15@gmail.com>
2022-11-20 01:45:57 +0600

    Learning JavaScript Asynchronous Programming is Action

--- AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson9.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson9.js
@@ -1,5 +1,7 @@
 // Promise API in Javascript 🎢
 
+/*
+
 const delay = (s) => new Promise((resolve) => setTimeout(resolve, s * 1000));
 
 delay(2).then(() => console.log("2 Second Delay 🦌"));
@@ -7,3 +9,13 @@ delay(1).then(() => console.log("1 Second Delay 🦌"));
 delay(3).then(() => console.log("3 Second Delay 🦌"));
 delay(2).then(() => console.log("2 Second Delay 🦌"));
 delay(5).then(() => console.log("5 Second Delay 🦌"));
+
+*/
+
+/*
+let p1 = Promise.resolve("RESOLVE");
+p1.then((v) => console.log(v));
+
+let p2 = Promise.reject("REJECT");
+p2.then((e) => console.log(e));
+*/
