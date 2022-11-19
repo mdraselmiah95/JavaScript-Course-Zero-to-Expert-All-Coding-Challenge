36b3d37d8c2d4358f7037852fc4a17e9cf32c472
mdraselmiah95 <russelroy15@gmail.com>
2022-11-20 01:11:27 +0600

    Learning JavaScript Asynchronous Programming is Action

--- AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson8.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson8.js
@@ -4,7 +4,20 @@ const BASE_URL = "https://jsonplaceholder.typicode.com/users";
 
 let res = fetch(BASE_URL)
   .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    return Promise.resolve("New Promise 👵");
+  })
+  .then((str) => {
+    console.log(str);
+    return Promise.resolve("Another Promise 🐥");
+  })
+  .then((another) => {
+    console.log(another);
+  })
+  .catch((err) => console.log(err));
+
+let res1 = fetch(BASE_URL)
+  .then((res) => res.json())
   .then((data) => console.log(data))
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch((err) => console.log(err));
