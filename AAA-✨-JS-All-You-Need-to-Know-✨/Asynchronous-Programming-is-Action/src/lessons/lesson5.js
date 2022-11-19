a715eba556ee55dd3cf9c822d7f1e05ee9eaf36f
mdraselmiah95 <russelroy15@gmail.com>
2022-11-19 21:54:03 +0600

    Learning JavaScript Asynchronous Programming is Action

--- AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson5.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson5.js
@@ -3,4 +3,15 @@
 let arr = [1, 2, 4, 5, 6, 7, 8, 9];
 
 let sqr = arr.map((v) => v * v);
-console.log(sqr);
+// console.log(sqr);
+
+function asyncMap(arr, cb) {
+  return arr.map((v) => {
+    setTimeout(cb.bind(null, v), 0);
+  });
+}
+
+let qbArr = asyncMap(arr, (v) => {
+  console.log(v);
+});
+// console.log(qbArr);
