2e38e8e88b526673fb0d89c72cf37b9d5dbe1228
mdraselmiah95 <russelroy15@gmail.com>
2022-11-19 23:26:39 +0600

    Learning JavaScript Asynchronous Programming is Action

--- AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson7.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson7.js
@@ -1,4 +1,9 @@
 // Introduction to Promise in Javascript 🍿
 
-let p1 = new Promise((resolve, reject) => {});
-console.log(p1);
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 3000, "Promise is resolve");
+});
+
+p1.then((v) => {
+  console.log(v);
+});
