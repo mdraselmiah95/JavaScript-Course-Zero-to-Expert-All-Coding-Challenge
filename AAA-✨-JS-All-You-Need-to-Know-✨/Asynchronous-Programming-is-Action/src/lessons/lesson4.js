30229bc7b91479224386bf5a0fb740c2a0ab4539
mdraselmiah95 <russelroy15@gmail.com>
2022-11-19 21:29:17 +0600

    Learning JavaScript Asynchronous Programming is Action

--- AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson4.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson4.js
@@ -6,11 +6,29 @@ xhr.open("get", "https://jsonplaceholder.typicode.com/users");
 xhr.onreadystatechange = function (e) {
   if (xhr.readyState === 4) {
     if (xhr.status === 200) {
-      console.log(xhr.response);
+      let users = JSON.parse(xhr.response);
+      console.log(users);
     } else {
       console.log(xhr.status);
     }
   }
 };
 
-xhr.send();
+// xhr.send();
+
+function getRequest(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("get", url);
+
+  xhr.onreadystatechange = function (e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        let response = JSON.parse(xhr.response);
+        callback(null, response);
+      } else {
+        callback(xhr.status, null);
+      }
+    }
+  };
+  xhr.send();
+}
