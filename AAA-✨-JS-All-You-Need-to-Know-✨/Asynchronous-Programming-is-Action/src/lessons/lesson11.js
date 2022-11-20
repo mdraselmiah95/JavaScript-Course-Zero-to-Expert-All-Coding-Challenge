36d54d25b9f976c0ad89cc8821d3f4de61fd0278
mdraselmiah95 <russelroy15@gmail.com>
2022-11-20 15:25:28 +0600

    Async Iterator in Javascript

--- AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson11.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Asynchronous-Programming-is-Action/src/lessons/lesson11.js
@@ -1,1 +1,17 @@
 // Async Iterator in Javascript 🍷
+
+let asyncIterable = {
+  [Symbol.asyncIterator]() {
+    let i = 0;
+    return {
+      next() {
+        if (i < 5) {
+          return Promise.resolve({
+            value: i++,
+            done: false,
+          });
+        }
+      },
+    };
+  },
+};
