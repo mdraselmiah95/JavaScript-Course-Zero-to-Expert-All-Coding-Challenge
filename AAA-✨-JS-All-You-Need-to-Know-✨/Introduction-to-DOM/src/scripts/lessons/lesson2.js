0559e8f019d1b66c0a369ace4e46765d28246b01
mdraselmiah95 <russelroy15@gmail.com>
2022-11-21 22:07:30 +0600

    Learning JavaScript Introduction to DOM

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson2.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson2.js
@@ -1,9 +1,22 @@
 // Create DOM Elements in Javascript 🪀
 
-let li = document.createElement("li");
-li.className = "list-group-item";
-li.setAttribute("title", "I am fourth Item.");
-li.innerHTML = "Four";
+// let li = document.createElement("li");
+// li.className = "list-group-item";
+// li.setAttribute("title", "I am fourth Item.");
+// li.innerHTML = "Four";
+
+// let list = document.getElementById("list");
+// list.appendChild(li);
+
+let li = createElement("li", "Four", "list-group-item");
+li.setAttribute("title", "I am number Item");
 
 let list = document.getElementById("list");
 list.appendChild(li);
+
+function createElement(tagName, innerHTML, className) {
+  let tag = document.createElement(tagName);
+  tag.innerHTML = innerHTML;
+  tag.className = className || "";
+  return tag;
+}
