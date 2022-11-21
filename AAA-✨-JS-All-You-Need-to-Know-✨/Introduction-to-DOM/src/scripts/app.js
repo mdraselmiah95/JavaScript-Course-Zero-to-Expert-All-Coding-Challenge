b4b59214ed4530085b5e2e6580082566f0f216e3
mdraselmiah95 <russelroy15@gmail.com>
2022-11-21 19:24:12 +0600

    Learning JavaScript Introduction to DOM

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/app.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/app.js
@@ -3,4 +3,16 @@ import "../styles/index.scss";
 let list = document.getElementById("list");
 
 let parent = list.parentElement;
-console.log(parent);
+//console.log(parent);
+
+let children = list.children;
+//console.log(children);
+
+//console.log(list.previousElementSibling);
+//console.log(list.nextElementSibling);
+
+let li = document.querySelector("li");
+//console.log(li.nextElementSibling);
+
+console.log(list.firstElementChild);
+console.log(list.lastElementChild);
