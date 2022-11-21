26920c059ba9348f3d0923001e674bd400df36b3
mdraselmiah95 <russelroy15@gmail.com>
2022-11-21 21:58:26 +0600

    Learning JavaScript Introduction to DOM

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson2.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson2.js
@@ -4,3 +4,6 @@ let li = document.createElement("li");
 li.className = "list-group-item";
 li.setAttribute = ("title", "I am fourth Item.");
 li.innerHTML = "Four";
+
+let list = document.getElementById("list");
+list.appendChild(li);
