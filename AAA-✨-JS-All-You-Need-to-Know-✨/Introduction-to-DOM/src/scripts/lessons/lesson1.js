d6d579db3acaf1e09649826821708122639ae5fb
mdraselmiah95 <russelroy15@gmail.com>
2022-11-21 20:06:53 +0600

    Learning JavaScript Introduction to DOM

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson1.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson1.js
@@ -12,5 +12,17 @@ let children = list.children;
 let li = document.querySelector("li");
 //console.log(li.nextElementSibling);
 
-console.log(list.firstElementChild);
-console.log(list.lastElementChild);
+// console.log(list.firstElementChild);
+// console.log(list.lastElementChild);
+
+// How to Loop Throw HTML Collections in Javascript 🪀
+
+let listItem = document.getElementsByTagName("li");
+
+let listItems = Array.from(listItem);
+console.log(listItems);
+
+listItems.forEach((li, index) => {
+  let text = li.innerHTML;
+  li.innerHTML = `(${index + 1}) ${text}`;
+});
