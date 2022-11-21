f6650bdbfe2f4c1c07220028a0d680df8af188cf
mdraselmiah95 <russelroy15@gmail.com>
2022-11-21 19:26:03 +0600

    Learning JavaScript Introduction to DOM

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/app.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/app.js
@@ -1,18 +1,3 @@
 import "../styles/index.scss";
 
-let list = document.getElementById("list");
-
-let parent = list.parentElement;
-//console.log(parent);
-
-let children = list.children;
-//console.log(children);
-
-//console.log(list.previousElementSibling);
-//console.log(list.nextElementSibling);
-
-let li = document.querySelector("li");
-//console.log(li.nextElementSibling);
-
-console.log(list.firstElementChild);
-console.log(list.lastElementChild);
+import "./lessons/lesson1";
---
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson1.js
@@ -0,0 +1,16 @@
+let list = document.getElementById("list");
+
+let parent = list.parentElement;
+//console.log(parent);
+
+let children = list.children;
+//console.log(children);
+
+//console.log(list.previousElementSibling);
+//console.log(list.nextElementSibling);
+
+let li = document.querySelector("li");
+//console.log(li.nextElementSibling);
+
+console.log(list.firstElementChild);
+console.log(list.lastElementChild);
