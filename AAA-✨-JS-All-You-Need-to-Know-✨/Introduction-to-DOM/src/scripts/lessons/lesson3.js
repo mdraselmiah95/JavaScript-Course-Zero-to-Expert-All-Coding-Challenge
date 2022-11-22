eba256958dbf568b3b32ec3c9913fcba733ff920
mdraselmiah95 <russelroy15@gmail.com>
2022-11-22 19:38:22 +0600

    Learning JavaScript Introduction to DOM

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson3.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson3.js
@@ -2,5 +2,19 @@
 
 let title = document.getElementById("title");
 title.style.color = "pink";
+// title.style.color = "#f00";
 title.style.fontStyle = "italic";
+title.style.fontSize = "2rem";
+title.style.background = "black";
 // console.log(title.style);
+
+let styleObj = {
+  background: "black",
+  fontSize: "1rem",
+  fontStyle: "italic",
+  color: "green",
+};
+
+let list = document.getElementById("list");
+
+[...list.children].forEach((li) => Object.assign(li.style, styleObj));
