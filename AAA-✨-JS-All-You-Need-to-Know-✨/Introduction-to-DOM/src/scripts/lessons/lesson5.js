6cc503dde8c4f81f83b3d4c04fad056bfe6abd5b
mdraselmiah95 <russelroy15@gmail.com>
2022-11-22 22:58:56 +0600

    Learning JavaScript Introduction to DOM

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson5.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson5.js
@@ -1,1 +1,10 @@
 // Input Box Event Handling in JavaScript 🤽‍♀️
+
+let name = document.getElementById("name");
+
+name.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    console.log(e.target.value);
+    e.target.value = "";
+  }
+});
