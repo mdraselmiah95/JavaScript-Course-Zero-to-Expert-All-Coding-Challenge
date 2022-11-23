4b6e3b518852b84b044232eb8454f9f5ea877798
mdraselmiah95 <russelroy15@gmail.com>
2022-11-23 23:02:36 +0600

    Learning JavaScript Form Handling in DOM

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson6.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson6.js
@@ -4,4 +4,7 @@ let form = document.getElementById("form");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  [...this.elements].forEach((el) => {
+    console.log(el.type, el.name, el.value);
+  });
 });
