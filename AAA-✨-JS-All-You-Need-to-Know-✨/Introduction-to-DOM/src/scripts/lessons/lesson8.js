1d1b8aa8583f41f0f1f36137f0146f45bf6daa76
mdraselmiah95 <russelroy15@gmail.com>
2022-11-24 23:39:50 +0600

    Learning JavaScript Ajax Post

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson8.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson8.js
@@ -1,10 +1,33 @@
 // Ajax Post Request in JavaScript 📽️
 
-const URL = "https://jsonplaceholder.typicode.com/users";
+const URL = "https://jsonplaceholder.typicode.com/posts";
 
 let postForm = document.getElementById("post-form");
 let posts = document.getElementById("new-post");
 
 postForm.addEventListener("click", function (e) {
   e.preventDefault();
+
+  let title = this.title.value || undefined;
+  let body = this.body.value || undefined;
+
+  if (title && body) {
+    let postObj = {
+      userId: 100,
+      title,
+      body,
+    };
+
+    // post
+
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-type": "Application/JSON",
+      },
+      body: JSON.stringify(postObj),
+    });
+  } else {
+    alert("Please provide Every Details. 💢");
+  }
 });
