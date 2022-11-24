46b8618802006be30770033ce40611468b4f8718
mdraselmiah95 <russelroy15@gmail.com>
2022-11-24 23:27:04 +0600

    Learning JavaScript Ajax Post

--- AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson8.js
+++ AAA-✨-JS-All-You-Need-to-Know-✨/Introduction-to-DOM/src/scripts/lessons/lesson8.js
@@ -4,3 +4,7 @@ const URL = "https://jsonplaceholder.typicode.com/users";
 
 let postForm = document.getElementById("post-form");
 let posts = document.getElementById("new-post");
+
+postForm.addEventListener("click", function (e) {
+  e.preventDefault();
+});
